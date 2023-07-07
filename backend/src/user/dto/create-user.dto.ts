93b3d2174e4749e4461e43092a0c829ea4410d2a
José Manuel <josesanc@student.42malaga.com>
2023-07-07 12:46:07 +0200

    🗃️ Database chats and messages defined

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,14 +1,26 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import {
+  IsDefined,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString
+} from 'class-validator';
 
 export class CreateUserDto {
+  @IsDefined()
   @IsNotEmpty()
   username: string;
 
+  @IsDefined()
+  @IsString()
   @IsNotEmpty()
   password: string;
 
+  @IsDefined()
   @IsEmail()
+  @IsString()
   email: string
 
+  @IsOptional()
   avatar: File;
 }
--- backend/src/user/dto/info-user.dto.ts
+++ backend/src/user/dto/info-user.dto.ts
@@ -1,5 +1,3 @@
-
-
 export class UserBasicInfoDto {
   id: number;
   username: string;
