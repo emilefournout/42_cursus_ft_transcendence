ebf6b0b9d5f5975ceb8675c4212e034395871e93
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-07-12 16:36:49 +0200

    42Api button onClick.

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -39,17 +39,14 @@ export function Login() {
         <h2 className="txt txt-shadow-bot txt-shadow-blu">
           A Transcendence Project for 42
         </h2>
-        <div>
-          <a
-            href={`https://api.intra.42.fr/oauth/authorize?client_id=${
+        <button className="btn btn-bottom" onClick={() => {
+            window.location.href =`https://api.intra.42.fr/oauth/authorize?client_id=${
               process.env.REACT_APP_INTRA_UID
             }&redirect_uri=${encodeURI(
               process.env.REACT_APP_REDIRECT_URI ?? ""
-            )}&response_type=code`}
-          >
+            )}&response_type=code`}}>
             Login with 42
-          </a>
-        </div>
+        </button>
       </div>
 
       {/*<button onClick={() => setShow2FA(!show2FA)}>
