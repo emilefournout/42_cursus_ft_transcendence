0e8833b9ac86480a0c0754aa2193b229207a0fef
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-07-14 10:32:54 +0200

    Fixed button height class.

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -10,7 +10,7 @@ export function Login() {
         <h2 className="txt txt-shadow-bot txt-shadow-blu">
           A Transcendence Project for 42
         </h2>
-        <button className="btn btn-bottom" onClick={() => {
+        <button className="btn btn-bottom btn-fixed-height" onClick={() => {
             window.location.href =`https://api.intra.42.fr/oauth/authorize?client_id=${
               process.env.REACT_APP_INTRA_UID
             }&redirect_uri=${encodeURI(
