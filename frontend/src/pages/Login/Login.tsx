d253425fcb8a312fcb22cfbfc61be053877eafe1
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-07-14 17:15:50 +0200

    Maximum responsivity login page B).

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -5,12 +5,12 @@ export function Login() {
 
   return (
       <>
-        <h1 className="txt txt-shadow-top">PONG</h1>
+        <h1 id="login-h1" className="txt txt-shadow-top">PONG</h1>
         <div className="underline"></div>
-        <h2 className="txt txt-shadow-bot txt-shadow-blu">
+        <h2 id="login-h2" className="txt txt-shadow-bot txt-shadow-blu">
           A Transcendence Project for 42
         </h2>
-        <button className="btn btn-bottom btn-fixed-height" onClick={() => {
+        <button className="btn btn-bottom btn-fixed-height responsive-button" onClick={() => {
             window.location.href =`https://api.intra.42.fr/oauth/authorize?client_id=${
               process.env.REACT_APP_INTRA_UID
             }&redirect_uri=${encodeURI(
