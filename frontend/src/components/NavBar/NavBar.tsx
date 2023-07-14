9fe17df5fffeda2f8513131e6fb23eb1dd15458f
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-07-14 17:33:53 +0200

    Super responsive navbar B))))).

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -12,8 +12,8 @@ export function NavBar() {
           <a className="nav-icons"><img src={chatsIcon}></img></a>
           <a className="nav-icons"><img src={userIcon}></img></a>
         </div>
-        <div className=""><h2 className="txt txt-shadow-top">PONG</h2></div>
-        <button id="nav-right-side" className="btn btn-bottom-left">Play</button>
+        <h2 className="txt txt-shadow-top nav-responsive-text">PONG</h2>
+        <button id="nav-right-side" className="btn btn-bottom-left nav-responsive-text">Play</button>
       </nav>
   );
 }
