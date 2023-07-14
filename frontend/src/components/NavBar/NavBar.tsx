5e099d2f2a08faf1c1eb82fab5f36382392e132b
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-07-14 16:04:17 +0200

    Transform navbar into component.

---
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./NavBar.css"
+import settingsIcon from "../../common/config.svg"
+import chatsIcon from "../../common/chats.svg"
+import userIcon from "../../common/user.svg"
+
+export function NavBar() {
+  return (
+      <nav>
+        <div id="nav-left-side">
+          <a className="nav-icons"><object data={settingsIcon}></object></a>
+          <a className="nav-icons"><object data={chatsIcon}></object></a>
+          <a className="nav-icons"><object data={userIcon}></object></a>
+        </div>
+        <div className=""><h2 className="txt txt-shadow-top">PONG</h2></div>
+        <button id="nav-right-side" className="btn btn-bottom-left">Play</button>
+      </nav>
+  );
+}
--- frontend/src/pages/Chat/Chat.tsx
+++ frontend/src/pages/Chat/Chat.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 import Chat from "../../components/Chat";
-import settingsIcon from "../../common/config.svg"
-import chatsIcon from "../../common/chats.svg"
-import userIcon from "../../common/user.svg"
+import { NavBar } from "../../components/NavBar/NavBar";
 
 export function ChatPage() {
   return (
     <>
-      <nav>
-        <div id="nav-left-side">
-          <a className="nav-icons"><object data={settingsIcon}></object></a>
-          <a className="nav-icons"><object data={chatsIcon}></object></a>
-          <a className="nav-icons"><object data={userIcon}></object></a>
-        </div>
-        <div className=""><h2 className="txt txt-shadow-top">PONG</h2></div>
-        <button id="nav-right-side" className="btn btn-bottom-left">Play</button>
-      </nav>
+      <NavBar />
+      {/* <Chat /> */}
     </>
-    // <Chat />
   );
 }
