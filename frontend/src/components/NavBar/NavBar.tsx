7ef47d64fcb9897dce1a1eb8df1f4a27616740b9
José Manuel <josesanc@student.42malaga.com>
2023-07-19 15:35:36 +0200

    ✨ Added links on navbar

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -9,9 +9,15 @@ export function NavBar() {
   return (
       <nav>
         <div id="nav-left-side">
-          <a className="nav-icons"><img src={settingsIcon}></img></a>
-          <a className="nav-icons"><img src={chatsIcon}></img></a>
-          <a className="nav-icons"><img src={userIcon}></img></a>
+          <Link to="/settings" className="nav-icons">
+          <img src={settingsIcon}></img>
+          </Link>
+          <Link to="/chats" className="nav-icons">
+          <img src={chatsIcon}></img>
+          </Link>
+          <Link to="/home" className="nav-icons">
+            <img src={userIcon}></img>
+          </Link>
         </div>
         <h2 className="txt txt-shadow-top nav-responsive-text">PONG</h2>
         <Link to="/game">
