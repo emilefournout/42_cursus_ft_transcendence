34faf9cc1c8de8d5454d3868e05d4bd29e0e2094
emile_frn <emile.fournout@gmail.com>
2023-07-22 21:31:00 +0200

    add game socket printing in front

--- frontend/src/pages/Game/GamePlayPage/GameCanvas.tsx
+++ frontend/src/pages/Game/GamePlayPage/GameCanvas.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef } from "react";
+import { gameSocket } from "../../../services/socket";
+import { useParams } from "react-router-dom";
+import Cookies from "js-cookie";
 
 export interface GameCanvasProps {
   width: number;
@@ -14,48 +17,90 @@ export interface GameCanvasProps {
 }
 
 export function GameCanvas(props: GameCanvasProps) {
-  const padWallSeparation = 20
-  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const padWallSeparation = 20;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    if (!canvasRef.current)
-      return
-    const canvas: HTMLCanvasElement = canvasRef.current
-    const ctx = canvas.getContext('2d')
+    if (!canvasRef.current) return;
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    const ctx = canvas.getContext("2d");
 
     if (ctx) {
       // Draw board
-      ctx.fillStyle = "black"
-      ctx.fillRect(0, 0, props.width, props.height)
+      ctx.fillStyle = "black";
+      ctx.fillRect(0, 0, props.width, props.height);
 
       // Draw middle line
-      ctx.setLineDash([15, 10])
-      ctx.beginPath()
-      ctx.moveTo(props.width / 2, 0)
-      ctx.lineTo(props.width / 2, props.height)
-      ctx.strokeStyle = "white"
-      ctx.lineWidth = 5
-      ctx.stroke()
+      ctx.setLineDash([15, 10]);
+      ctx.beginPath();
+      ctx.moveTo(props.width / 2, 0);
+      ctx.lineTo(props.width / 2, props.height);
+      ctx.strokeStyle = "white";
+      ctx.lineWidth = 5;
+      ctx.stroke();
 
       // Draw left pad
-      ctx.fillStyle = "white"
-      ctx.fillRect(padWallSeparation, props.leftPad, props.padWidth, props.padHeight)
+      ctx.fillStyle = "white";
+      ctx.fillRect(
+        padWallSeparation,
+        props.leftPad,
+        props.padWidth,
+        props.padHeight
+      );
       // Draw right pad
       // ctx.fillStyle = "white"
-      ctx.fillRect(props.width - padWallSeparation - props.padWidth, props.rightPad, props.padWidth, props.padHeight)
+      ctx.fillRect(
+        props.width - padWallSeparation - props.padWidth,
+        props.rightPad,
+        props.padWidth,
+        props.padHeight
+      );
 
       // Draw ball
       // ctx.fillStyle = "white"
-      ctx.beginPath()
-      ctx.arc(props.ballX, props.ballY, props.ballRadius, 0, 2 * Math.PI)
-      ctx.fill()
-      console.log("BALL", props.ballX, props.ballY)
+      ctx.beginPath();
+      ctx.arc(props.ballX, props.ballY, props.ballRadius, 0, 2 * Math.PI);
+      ctx.fill();
+      console.log("BALL", props.ballX, props.ballY);
     }
-  })
+  });
+
+  const id = useParams();
+  const access_token: string | undefined = Cookies.get("42token");
+
+  useEffect(() => {
+    function handleKeyDown(event: any) {
+      if (event.keyCode === 38) {
+        console.log("send keyup");
+        gameSocket.emit("move_user", {
+          playerId: access_token,
+          gameId: id,
+          direction: "up",
+        });
+      } else if (event.keyCode === 40) {
+        gameSocket.emit("move_user", {
+          playerId: access_token,
+          gameId: id,
+          direction: "down",
+        });
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return function cleanup() {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [access_token, id]);
 
   return (
     <>
-      <canvas ref={canvasRef} width={props.width} height={props.height} style={{border: "2px solid black"}}></canvas>
+      <canvas
+        ref={canvasRef}
+        width={props.width}
+        height={props.height}
+        style={{ border: "2px solid black" }}
+      ></canvas>
     </>
-  )
+  );
 }
--- frontend/src/pages/Game/GamePlayPage/GamePlayPage.tsx
+++ frontend/src/pages/Game/GamePlayPage/GamePlayPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { GameCanvas, GameCanvasProps } from "./GameCanvas";
+import { gameSocket } from "../../../services/socket";
 
 export function GamePlayPage() {
   const boardState: GameCanvasProps = {
@@ -11,23 +12,31 @@ export function GamePlayPage() {
     socket: null,
     leftPad: 100,
     rightPad: 150,
-    ballX: 200,
+    ballX: 100,
     ballY: 150,
-  }
-  const [state, setState] = useState(boardState)
+  };
 
-  // let start = Date.now()
+  const [state, updateGameState] = useState(boardState);
+
+  gameSocket.on("update", (data: any) => {
+    updateGameState(data);
+  });
+
+  // let start = Date.now();
   // requestAnimationFrame(function animate(timestamp) {
-  //   let interval = Date.now() - start
-  //   setState(state => ({...state, ballX: state.ballX + interval / 100}))
-  //   console.log(interval)
-  //   if (interval < 50) requestAnimationFrame(animate)
-  // })
+  //   let interval = Date.now() - start;
+  //   setState((state) => ({ ...state, ballX: state.ballX + interval / 100 }));
+  //   console.log(interval);
+  //   if (interval < 50) requestAnimationFrame(animate);
+  // });
   // setInterval(() => {
-  //   setState(state => ({...state,
-  //     ballX: state.ballX + 1, ballY: state.ballY + 1}))
+  //   setState((state) => ({
+  //     ...state,
+  //     ballX: state.ballX + 1,
+  //     ballY: state.ballY + 1,
+  //   }));
   //   // console.log('update', state.ballX)
-  // }, 5000)
+  // }, 5000);
 
   return (
     <>
@@ -36,4 +45,3 @@ export function GamePlayPage() {
     </>
   );
 }
-
