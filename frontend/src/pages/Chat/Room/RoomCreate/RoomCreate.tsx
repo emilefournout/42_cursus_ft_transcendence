947ea0d96dc35915cd1dea1655956b65b993e5d0
senari <onextthenitromancer@gmail.com>
2023-07-31 21:12:51 +0200

    Different onhover for visbtns.

--- frontend/src/pages/Chat/Room/RoomCreate/RoomCreate.tsx
+++ frontend/src/pages/Chat/Room/RoomCreate/RoomCreate.tsx
@@ -95,7 +95,7 @@ export function RoomCreate() {
 		<div className="wrapper-new-room">
 			<h2 className="txt-light">Create a new room</h2>
 			<h3 className="txt-light mini-title">Visibility:</h3>
-			<div className="wrapper-row align-items-center">
+			<div className="wrapper-row wrapper-vis-btns">
 				<VisibilityButton
 					type={Visibility.PUBLIC}
 					selected={selected}
