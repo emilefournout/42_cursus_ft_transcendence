654106fcbf1d7113abf4103c4a31cd882cb79421
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-01 16:14:07 +0200

    Input bar.

--- frontend/src/pages/Chat/Room/RoomInput/RoomInput.tsx
+++ frontend/src/pages/Chat/Room/RoomInput/RoomInput.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "./RoomInput.css"
+import SendIcon from "./SendIcon.svg"
 
 export function RoomInput() {
   return (
-    <>
-      <h1>TextInput</h1>
-      <button>Send</button>
-    </>
+    <div className="wrapper-room-input">
+      <input></input>
+      <div><img 
+        className="nav-icons"
+        src={SendIcon}
+      ></img></div>
+    </div>
   );
 }
