c913a78ed246a2bdc37653d0da1b0edf7e4b3b57
adri <adripbar03@gmail.com>
2023-08-12 01:52:37 +0200

    Game id validation

--- frontend/src/pages/Game/GamePlayPage/GamePlayPage.tsx
+++ frontend/src/pages/Game/GamePlayPage/GamePlayPage.tsx
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { GameCanvas, GameCanvasProps } from "./GameCanvas";
 import { GameSocket } from "../../../services/socket";
+import { useNavigate, useParams } from "react-router-dom";
+
+enum GameExistState {
+  Waiting,
+  NotFound,
+  Play
+}
 
 export function GamePlayPage() {
+  const [gameExistState, setGameExistState] = useState(GameExistState.Waiting);
+  const {id} = useParams();
+  const navigate = useNavigate();
   const gameSocket = GameSocket.getInstance().socket;
   const boardState: GameCanvasProps = {
     width: 600,
@@ -21,6 +31,24 @@ export function GamePlayPage() {
   const [player2Score, updatePlayer2Score] = useState(0);
 
   const [showModal, setShowModal] = useState(null);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BACKEND}/game/info/${id}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        
+      },
+    }).then((response) => {
+      if (response.ok) {
+        setGameExistState(GameExistState.Play);
+      }
+      else {
+        setGameExistState(GameExistState.NotFound);
+      }
+    })
+    
+  }, [id])
 
   useEffect(() => {
     gameSocket.off("update");
@@ -44,18 +72,26 @@ export function GamePlayPage() {
 
   return (
     <>
-      <h1 className="title">Game</h1>
-      <div className="title">
-        {player1Score} - {player2Score}
-      </div>
-      <GameCanvas {...state} />
-      {showModal !== null && (
-        <div className="modal">
-          <div className="modal-content">
-            <p>{showModal} wins</p>
-            <button onClick={handleRedirect}>HOME</button>
+      {gameExistState === GameExistState.Waiting ? (
+        <div>Waiting</div>
+      ) : gameExistState === GameExistState.NotFound ? (
+        <div>Not Found</div>
+      ) : (
+        <>
+          <h1 className="title">Game</h1>
+          <div className="title">
+            {player1Score} - {player2Score}
           </div>
-        </div>
+          <GameCanvas {...state} />
+          {showModal !== null && (
+            <div className="modal">
+              <div className="modal-content">
+                <p>{showModal} wins</p>
+                <button onClick={handleRedirect}>HOME</button>
+              </div>
+            </div>
+          )}
+        </>
       )}
     </>
   );
