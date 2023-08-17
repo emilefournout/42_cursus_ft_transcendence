ea8de08601b8e545b9e62c66224b7529e2c7ca0d
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-17 16:46:16 +0200

    Incomplete message styling.

--- frontend/src/pages/Chat/Room/Messages/Messages.tsx
+++ frontend/src/pages/Chat/Room/Messages/Messages.tsx
@@ -6,58 +6,60 @@ import { RoomInput } from "../RoomInput/RoomInput";
 import { BoardContext } from "../../../Board/Board";
 
 export interface MsgProps {
-  messages: Array<Msg>;
+	messages: Array<Msg>;
 }
+
 export interface Msg {
-  uuid: string;
-  text: string;
-  userId: number;
-  chatId: number;
+	uuid: string;
+	text: string;
+	userId: number;
+	chatId: number;
 }
+
 export function Messages() {
-  const [messages, setMessages] = useState<Array<Msg>>([]);
-  const { id } = useParams();
-  useEffect(() => {
-    {
-      /* fetch(`http://localhost:3000/chat/${id}/messages`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setMessages(data);
-      });*/
-      setMessages([
-        { uuid: "1", text: "hello", userId: 1, chatId: 1 },
-        { uuid: "2", text: "how are you ?", userId: 2, chatId: 1 },
-        { uuid: "3", text: "I am fine and you ?", userId: 1, chatId: 1 },
-      ]);
-    }
-    return () => {};
-  }, [id]);
-  const boardContext = React.useContext(BoardContext);
-  const myUserId = boardContext?.me.id;
-  return (
-    <>
-      {" "}
-      <div className="messages">
-        {messages.map((message: Msg) => {
-          {
-            /*Todo handle no user ID*/
-          }
-          if (!myUserId) throw new Error("No user id");
-          const isMyMessage =
-            message.userId === myUserId ? "message-right" : "message-left";
-          return (
-            <div className={isMyMessage} key={message.uuid}>
-              {message.text}
-            </div>
-          );
-        })}
-      </div>
-      <RoomInput />
-    </>
-  );
+	const [messages, setMessages] = useState<Array<Msg>>([]);
+	const { id } = useParams();
+	useEffect(() => {
+		{
+			/* fetch(`http://localhost:3000/chat/${id}/messages`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				setMessages(data);
+			});*/
+			setMessages([
+				{ uuid: "1", text: "hello", userId: 1, chatId: 1 },
+				{ uuid: "2", text: "how are you ?", userId: 2, chatId: 1 },
+				{ uuid: "3", text: "I am fine and you ?", userId: 1, chatId: 1 },
+			]);
+		}
+		return () => {};
+	}, [id]);
+	const boardContext = React.useContext(BoardContext);
+	const myUserId = boardContext?.me.id;
+	return (
+		<>
+			{" "}
+			<div className="wrapper-msgs">
+				{messages.map((message: Msg) => {
+					{
+						/*Todo handle no user ID*/
+					}
+					if (!myUserId) throw new Error("No user id");
+					const isMyMessage =
+						message.userId === myUserId ? "message message-right" : "message message-left";
+					return (
+						<div className={isMyMessage} key={message.uuid}>
+							{message.text}
+						</div>
+					);
+				})}
+			</div>
+			<RoomInput />
+		</>
+	);
 }
