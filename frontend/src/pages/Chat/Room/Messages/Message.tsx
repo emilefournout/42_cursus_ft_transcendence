599ce041433a34736a2372ada52102d7d83523b4
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-20 13:30:42 +0200

    Better date.

--- frontend/src/pages/Chat/Room/Messages/Message.tsx
+++ frontend/src/pages/Chat/Room/Messages/Message.tsx
@@ -2,17 +2,35 @@ import React from "react";
 import { Msg } from "./Messages";
 
 interface MessageProps {
-  message: Msg;
-  isMyMessage: string;
-  key: string;
+	message: Msg;
+	isMyMessage: boolean;
+	msgClasses: string;
+	key: string;
 }
 
 export function Message(props: MessageProps) {
-  const date = new Date(props.message.createdAt).toLocaleString();
-  return (
-    <div className={props.isMyMessage}>
-      {date + " | "}
-      {props.message.text}
-    </div>
-  );
+	const date = new Date(props.message.createdAt).toLocaleString().replace(", ", "\n");
+	return (
+		<>{
+			props.isMyMessage ? (
+				<div className="message-container">
+					<div className="wrapper-col message-date-right">
+						{date}
+					</div>
+					<div className={props.msgClasses}>
+						{props.message.text}
+					</div>
+				</div>
+			) : (
+				<div className="message-container">
+					<div className={props.msgClasses}>
+						{props.message.text}
+					</div>
+					<div className="wrapper-col message-date-left">
+						{date}
+					</div>
+				</div>
+			)
+		}</>
+	);
 }
--- frontend/src/pages/Chat/Room/Messages/Messages.tsx
+++ frontend/src/pages/Chat/Room/Messages/Messages.tsx
@@ -7,68 +7,70 @@ import { BoardContext } from "../../../Board/Board";
 import { ChatSocket } from "../../../../services/socket";
 
 export interface MsgProps {
-  messages: Array<Msg>;
+	messages: Array<Msg>;
 }
 
 export interface Msg {
-  uuid: string;
-  text: string;
-  createdAt: string;
-  userId: number;
-  chatId: number;
+	uuid: string;
+	text: string;
+	createdAt: string;
+	userId: number;
+	chatId: number;
 }
 
 export function Messages() {
-  const [messages, setMessages] = useState<Array<Msg>>([]);
-  const chatSocket = ChatSocket.getInstance().socket;
-  const { id } = useParams();
-  useEffect(() => {
-    chatSocket.emit("join_room", { chatId: id })
-    fetch(`${process.env.REACT_APP_BACKEND}/chat/${id}/messages`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setMessages(data);
-      });
+	const [messages, setMessages] = useState<Array<Msg>>([]);
+	const chatSocket = ChatSocket.getInstance().socket;
+	const { id } = useParams();
+	useEffect(() => {
+		chatSocket.emit("join_room", { chatId: id })
+		fetch(`${process.env.REACT_APP_BACKEND}/chat/${id}/messages`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				setMessages(data);
+			});
 
-    return () => {};
-  }, [id]);
+		return () => {};
+	}, [id]);
 
-  useEffect(() => {
-    chatSocket.off("receive_message");
-    chatSocket.on("receive_message", (data) => {
-      if (data.chatId === Number(id))
-        setMessages((msgs) => [...msgs, data]);
-    });
-  }, []);
+	useEffect(() => {
+		chatSocket.off("receive_message");
+		chatSocket.on("receive_message", (data) => {
+			if (data.chatId === Number(id))
+				setMessages((msgs) => [...msgs, data]);
+		});
+	}, []);
 
-  const boardContext = React.useContext(BoardContext);
-  const myUserId = boardContext?.me.id;
-  if (boardContext === undefined) return <>loading</>;
-  else {
-    return (
-      <>
-        <div className="wrapper-msgs">
-          {messages.map((message: Msg) => {
-            const isMyMessage =
-              message.userId === myUserId
-                ? "message message-right"
-                : "message message-left";
-            return (
-              <Message
-                message={message}
-                isMyMessage={isMyMessage}
-                key={message.uuid}
-              />
-            );
-          })}
-        </div>
-        <RoomInput chatSocket={chatSocket} />
-      </>
-    );
-  }
+	const boardContext = React.useContext(BoardContext);
+	const myUserId = boardContext?.me.id;
+	if (boardContext === undefined) return <>loading</>;
+	else {
+		return (
+			<>
+				<div className="wrapper-msgs">
+					{messages.map((message: Msg) => {
+						const isMyMessage = message.userId === myUserId;
+						const msgClasses = isMyMessage ?
+							"message-content message-content-right" :
+							"message-content message-content-left";
+
+						return (
+							<Message
+								message={message}
+								isMyMessage={isMyMessage}
+								msgClasses={msgClasses}
+								key={message.uuid}
+							/>
+						);
+					})}
+				</div>
+				<RoomInput chatSocket={chatSocket} />
+			</>
+		);
+	}
 }
