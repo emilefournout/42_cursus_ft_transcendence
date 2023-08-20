e907a60fc83e671c25e3414c6684e68e18752a73
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-20 11:36:32 +0200

    Conversation styling.

--- frontend/src/pages/Chat/ChatLeftBar/ChatLeftBar.tsx
+++ frontend/src/pages/Chat/ChatLeftBar/ChatLeftBar.tsx
@@ -5,49 +5,51 @@ import NewChatIcon from "./NewChatIcon.svg";
 import ReloadBlackIcon from "../../../common/reload_black.svg";
 import { Link, useNavigate } from "react-router-dom";
 import { ChatInfo } from "../Chat";
+
 export interface LeftBarProps {
-  chats: Array<ChatInfo>;
-  updateChats: () => void;
+	chats: Array<ChatInfo>;
+	updateChats: () => void;
 }
 
 export function LeftBar(props: LeftBarProps) {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  return (
-    <div id="lb-main-wrapper" className="wrapper-col">
-      <div id="lb-top-wrapper">
-        <span>Chats</span>
-        <Link to="/board/chats/create">
-          <img className="nav-icons" src={NewChatIcon} />
-        </Link>
-        <img
-          className="nav-icons"
-          src={ReloadBlackIcon}
-          onClick={props.updateChats}
-        />
-      </div>
-      {/* <Link to="/chats/room">*/}
-      <div id="lb-bot-wrapper">
-        {props.chats.length === 0 ? (
-          <div>You have no chats</div>
-        ) : (
-          props.chats.map((chat: ChatInfo) => {
-            return (
-              <div
-                key={chat.id}
-                onClick={() => {
-                  navigate(`/board/chats/${chat.id}`, {
-                    state: { chat: chat },
-                  });
-                }}
-              >
-                {chat.name ? chat.name : "No name"}
-              </div>
-            );
-          })
-        )}
-      </div>
-      {/*</Link>*/}
-    </div>
-  );
+	return (
+		<div id="lb-main-wrapper" className="wrapper-col">
+			<div id="lb-top-wrapper">
+				<span>Chats</span>
+				<Link to="/board/chats/create">
+					<img className="nav-icons" src={NewChatIcon} />
+				</Link>
+				<img
+					className="nav-icons"
+					src={ReloadBlackIcon}
+					onClick={props.updateChats}
+				/>
+			</div>
+			{/* <Link to="/chats/room">*/}
+			<div id="lb-bot-wrapper">
+				{props.chats.length === 0 ? (
+					<div>You have no chats</div>
+				) : (
+					props.chats.map((chat: ChatInfo) => {
+						return (
+							<div
+								className="wrapper-row wrapper-conversation"
+								key={chat.id}
+								onClick={() => {
+									navigate(`/board/chats/${chat.id}`, {
+										state: { chat: chat },
+									});
+								}}
+							>
+								{chat.name ? chat.name : "No name"}
+							</div>
+						);
+					})
+				)}
+			</div>
+			{/*</Link>*/}
+		</div>
+	);
 }
