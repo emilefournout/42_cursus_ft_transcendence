7dd74877daf16b797602728a9fceae4aa5d50894
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-21 18:31:07 +0200

    Today's last progress.

--- frontend/src/pages/UserProfile/UserProfile/Profile/Stats/Stats.tsx
+++ frontend/src/pages/UserProfile/UserProfile/Profile/Stats/Stats.tsx
@@ -1,29 +1,30 @@
 import React from "react";
 import { StatCards, StatType } from "./StatCards";
 import { ProfilePageContext } from "../../../UserProfilePage";
+import "../Profile.css";
 
 interface StatsProps {
-  wins: number;
-  loses: number;
-  id: number;
+	wins: number;
+	loses: number;
+	id: number;
 }
 export function Stats(props: StatsProps) {
-  const profilePageContext = React.useContext(ProfilePageContext);
-  const getRank = (): string | undefined => {
-    const userPos: number | undefined = profilePageContext.ranking?.findIndex(
-      (user) => user.id === props.id
-    );
-    if (userPos === undefined || userPos === -1) return undefined;
-    return "#" + (userPos + 1).toString();
-  };
-  const rank = getRank();
+	const profilePageContext = React.useContext(ProfilePageContext);
+	const getRank = (): string | undefined => {
+		const userPos: number | undefined = profilePageContext.ranking?.findIndex(
+			(user) => user.id === props.id
+		);
+		if (userPos === undefined || userPos === -1) return undefined;
+		return "#" + (userPos + 1).toString();
+	};
+	const rank = getRank();
 
-  return (
-    <div style={{ border: "5px solid" }}>
-      <h1>Stats:</h1>
-      <StatCards statType={StatType.win} statValue={props.wins?.toString()} />
-      <StatCards statType={StatType.lose} statValue={props.loses?.toString()} />
-      {rank && <StatCards statType={StatType.rank} statValue={rank} />}
-    </div>
-  );
+	return (
+		<div id="stats-card">
+			<h1>Stats:</h1>
+			<StatCards statType={StatType.win} statValue={props.wins?.toString()} />
+			<StatCards statType={StatType.lose} statValue={props.loses?.toString()} />
+			{rank && <StatCards statType={StatType.rank} statValue={rank} />}
+		</div>
+	);
 }
