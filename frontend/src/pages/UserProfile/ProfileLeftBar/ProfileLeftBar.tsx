5817a17d4c39af81d43e385ce861b6279160ec33
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-21 16:03:52 +0200

    Profile tool bar beginnings.

--- frontend/src/pages/UserProfile/ProfileLeftBar/ProfileLeftBar.tsx
+++ frontend/src/pages/UserProfile/ProfileLeftBar/ProfileLeftBar.tsx
@@ -5,35 +5,35 @@ import "./ProfileLeftBar.css";
 import { RequestType } from "../UserProfilePage";
 
 export interface ProfileBarContextArgs {
-  setRequestsType: React.Dispatch<React.SetStateAction<RequestType>>;
-  requestsType: RequestType;
+	setRequestsType: React.Dispatch<React.SetStateAction<RequestType>>;
+	requestsType: RequestType;
 }
 
 export const ProfileBarContext = React.createContext<ProfileBarContextArgs>(
-  {} as ProfileBarContextArgs
+	{} as ProfileBarContextArgs
 );
 export function ProfileLeftBar() {
-  const [requestsType, setRequestsType] = useState<RequestType>(
-    RequestType.enabled
-  );
+	const [requestsType, setRequestsType] = useState<RequestType>(
+		RequestType.enabled
+	);
 
-  return (
-    <>
-      <ProfileBarContext.Provider
-        value={
-          {
-            requestsType: requestsType,
-            setRequestsType: setRequestsType,
-          } as ProfileBarContextArgs
-        }
-      >
-        <div className="profileLeftBar">
-          {/* My profile button*/}
+	return (
+		<div id="prof-left-bar-container">
+			<ProfileBarContext.Provider
+				value={
+					{
+						requestsType: requestsType,
+						setRequestsType: setRequestsType,
+					} as ProfileBarContextArgs
+				}
+			>
+				<div id="prof-left-bar" className="wrapper-col">
+					{/* My profile button*/}
 
-          <ProfileToolBar />
-          <FriendList />
-        </div>
-      </ProfileBarContext.Provider>
-    </>
-  );
+					<ProfileToolBar />
+					<FriendList />
+				</div>
+			</ProfileBarContext.Provider>
+		</div>
+	);
 }
--- frontend/src/pages/UserProfile/ProfileLeftBar/ProfileToolBar/ProfileToolBar.tsx
+++ frontend/src/pages/UserProfile/ProfileLeftBar/ProfileToolBar/ProfileToolBar.tsx
@@ -4,93 +4,99 @@ import { ProfilePageContext, RequestType } from "../../UserProfilePage";
 import { RequestTypeButton } from "./RequestTypeButton";
 import { useNavigate } from "react-router-dom";
 import ReloadBlackIcon from "../../../../common/reload_honey.svg";
+import AddFriendIcon from "./AddFriendIcon.svg";
+
 export function ProfileToolBar() {
-  const [newFriend, setNewFriend] = useState<string>("");
-  const profileContext = React.useContext(ProfilePageContext);
-  const navigate = useNavigate();
-  const addFriend = () => {
-    fetch(`${process.env.REACT_APP_BACKEND}/user/info/username/${newFriend}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-    })
-      .then((response) => {
-        if (response.ok) return response.json();
-        else {
-          alert("User not found");
-          throw new Error();
-        }
-      })
-      .then((data) =>
-        fetch(`${process.env.REACT_APP_BACKEND}/user/friends/send/${data.id}`, {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-          },
-        })
-      )
-      .then((response) => {
-        if (response.ok) {
-          alert("Friend request sent");
-          profileContext.updateFriends();
-          clearState();
-        } else {
-          response.json().then((data) => alert(data.message));
-          throw new Error();
-        }
-      })
-      .catch((error) => console.log(error));
-  };
+	const [newFriend, setNewFriend] = useState<string>("");
+	const profileContext = React.useContext(ProfilePageContext);
+	const navigate = useNavigate();
+	const addFriend = () => {
+		fetch(`${process.env.REACT_APP_BACKEND}/user/info/username/${newFriend}`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+			},
+		})
+			.then((response) => {
+				if (response.ok) return response.json();
+				else {
+					alert("User not found");
+					throw new Error();
+				}
+			})
+			.then((data) =>
+				fetch(`${process.env.REACT_APP_BACKEND}/user/friends/send/${data.id}`, {
+					method: "POST",
+					headers: {
+						Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+					},
+				})
+			)
+			.then((response) => {
+				if (response.ok) {
+					alert("Friend request sent");
+					profileContext.updateFriends();
+					clearState();
+				} else {
+					response.json().then((data) => alert(data.message));
+					throw new Error();
+				}
+			})
+			.catch((error) => console.log(error));
+	};
 
-  const clearState = () => {
-    setNewFriend("");
-  };
+	const clearState = () => {
+		setNewFriend("");
+	};
 
-  return (
-    <div style={{ display: "flex", flexDirection: "column", gap: "15px" }}>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          gap: "10px",
-        }}
-      >
-        <div>
-          <button onClick={() => navigate("/board/userAccount")}>
-            my profil
-          </button>
-        </div>
-        <div>
-          <button onClick={() => navigate("/board/userAccount/ranking")}>
-            ranking
-          </button>
-        </div>
-      </div>
-      <div className="input-add-friend">
-        <input
-          value={newFriend}
-          type="text"
-          placeholder="add new friend"
-          onChange={(e) => setNewFriend(e.target.value)}
-        />
-        <button onClick={addFriend}>+</button>
-        <div>
-          <img
-            className="nav-icons"
-            src={ReloadBlackIcon}
-            onClick={profileContext.updateFriends}
-          />
-        </div>
-      </div>
-      <div className="request-type-buttons">
-        <div>
-          <RequestTypeButton type={RequestType.enabled} />
-        </div>
-        <div>
-          <RequestTypeButton type={RequestType.pending} />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div id="prof-tool-bar-container">
+
+
+			{/* THE MY PROFILE IS REDUNDANT SINCE THE USER CAN CLICK THENT ICON IN THE NAVBAR. THE RANKING ONE IS BETTER PLACED INSIDE THE STATS SUBWINDOW  */}
+
+			{/* <div
+			  *     style={{
+			  *         display: "flex",
+			  *         flexDirection: "row",
+			  *         gap: "10px",
+			  *     }}
+			  * >
+			  *     <button onClick={() => navigate("/board/userAccount")}>
+			  *         my profil
+			  *     </button>
+			  *     <button onClick={() => navigate("/board/userAccount/ranking")}>
+			  *         ranking
+			  *     </button>
+			  * </div> */}
+
+
+			<div className="input-add-friend wrapper-row">
+				<input
+					value={newFriend}
+					type="text"
+					placeholder="Add Friend!"
+					onChange={(e) => setNewFriend(e.target.value)}
+				/>
+				<img
+					className="nav-icons"
+					src={AddFriendIcon}
+					onClick={addFriend}
+				/>
+				<img
+					className="nav-icons"
+					src={ReloadBlackIcon}
+					onClick={profileContext.updateFriends}
+				/>
+			</div>
+			<div className="request-type-buttons">
+				<div>
+					<RequestTypeButton type={RequestType.enabled} />
+				</div>
+				<div>
+					<RequestTypeButton type={RequestType.pending} />
+				</div>
+			</div>
+		</div>
+	);
 }
