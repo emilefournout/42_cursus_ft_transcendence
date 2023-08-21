080d883e6c44e5a8e842398cf09209a830f163de
José Manuel <josesanc@student.42malaga.com>
2023-08-21 22:15:12 +0200

    🔒️ Validation on JWT user token

--- backend/src/auth/strategy/jwt.strategy.ts
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -1,20 +1,29 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { JwtPayload } from "../interface/jwtpayload.dto";
+import { UserService } from "src/user/user.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt')
 {
-    constructor(private configService: ConfigService){
+    constructor(
+        private configService: ConfigService,
+        private userService: UserService)
+        {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: configService.get('JWT_SECRET')
         })
     }
 
-    validate(payload: JwtPayload) {
+    async validate(payload: JwtPayload) {
+        const user = await this.userService.findUserById(payload.sub)
+        if (!user) 
+            throw new UnauthorizedException('Token id not valid')
+        if (user.username != payload.username) 
+            throw new UnauthorizedException('Username does not correspond to user identified by id');
         return payload
     }
 }
