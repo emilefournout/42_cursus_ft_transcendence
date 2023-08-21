7c9469a835753dc21ad8781282fbf755a6b242b8
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-21 14:58:35 +0200

    Messages scrolling and display fixed.

--- frontend/src/pages/Chat/Room/Messages/Messages.tsx
+++ frontend/src/pages/Chat/Room/Messages/Messages.tsx
@@ -7,87 +7,89 @@ import { BoardContext, User } from "../../../Board/Board";
 import { ChatSocket } from "../../../../services/socket";
 
 export interface MsgProps {
-  messages: Array<Msg>;
+	messages: Array<Msg>;
 }
 
 export interface Msg {
-  uuid: string;
-  text: string;
-  createdAt: string;
-  userId: number;
-  chatId: number;
+	uuid: string;
+	text: string;
+	createdAt: string;
+	userId: number;
+	chatId: number;
 }
 
 export function Messages() {
-  const [messages, setMessages] = useState<Array<Msg> | undefined>(undefined);
+	const [messages, setMessages] = useState<Array<Msg> | undefined>(undefined);
 
-  const chatSocket = ChatSocket.getInstance().socket;
-  const { id } = useParams();
-  const navigate = useNavigate();
-  useEffect(() => {
-    chatSocket.emit("join_room", { chatId: id });
-    fetch(`${process.env.REACT_APP_BACKEND}/chat/${id}/messages`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-    })
-      .then((response) => {
-        if (!response.ok) throw new Error("conv not found");
-        return response.json();
-      })
-      .then((data) => data as Msg[])
-      .then((data) => {
-        setMessages(data);
-      })
-      .catch((error) => {
-        setMessages(undefined);
-        console.log(error);
-      });
-    return () => {};
-  }, [chatSocket, id, messages, navigate]);
+	const chatSocket = ChatSocket.getInstance().socket;
+	const { id } = useParams();
+	const navigate = useNavigate();
+	useEffect(() => {
+		chatSocket.emit("join_room", { chatId: id });
+		fetch(`${process.env.REACT_APP_BACKEND}/chat/${id}/messages`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+			},
+		})
+			.then((response) => {
+				if (!response.ok) throw new Error("conv not found");
+				return response.json();
+			})
+			.then((data) => data as Msg[])
+			.then((data) => {
+				setMessages(data);
+			})
+			.catch((error) => {
+				setMessages(undefined);
+				console.log(error);
+			});
+		return () => {};
+	}, [chatSocket, id, messages, navigate]);
 
-  useEffect(() => {
-    if (messages === undefined) return;
-    chatSocket.off("receive_message");
-    chatSocket.on("receive_message", (data) => {
-      if (data.chatId === Number(id)) setMessages((msgs) => [...msgs!, data]);
-    });
-  }, [chatSocket, id, messages]);
+	useEffect(() => {
+		if (messages === undefined) return;
+		chatSocket.off("receive_message");
+		chatSocket.on("receive_message", (data) => {
+			if (data.chatId === Number(id)) setMessages((msgs) => [...msgs!, data]);
+		});
+	}, [chatSocket, id, messages]);
 
-  const boardContext = React.useContext(BoardContext);
-  const myUserId = boardContext?.me.id;
+	const boardContext = React.useContext(BoardContext);
+	const myUserId = boardContext?.me.id;
 
-  if (messages === undefined)
-    return (
-      <>
-        conv not found
-        <button onClick={() => navigate("/board/chats")}>back</button>
-      </>
-    );
-  else if (boardContext === undefined) return <>loading</>;
-  else {
-    return (
-      <>
-        <div className="wrapper-msgs">
-          {messages.map((message: Msg) => {
-            const isMyMessage = message.userId === myUserId;
-            const msgClasses = isMyMessage
-              ? "message-content message-content-right"
-              : "message-content message-content-left";
+	if (messages === undefined)
+		return (
+			<>
+				conv not found
+				<button onClick={() => navigate("/board/chats")}>back</button>
+			</>
+		);
+	else if (boardContext === undefined) return <>loading</>;
+	else {
+		return (
+			<>
+				<div className="wrapper-msgs">
+					<div>
+						{messages.map((message: Msg) => {
+							const isMyMessage = message.userId === myUserId;
+							const msgClasses = isMyMessage
+								? "message-content message-content-right"
+								: "message-content message-content-left";
 
-            return (
-              <Message
-                message={message}
-                isMyMessage={isMyMessage}
-                msgClasses={msgClasses}
-                key={message.uuid}
-              />
-            );
-          })}
-        </div>
-        <RoomInput chatSocket={chatSocket} />
-      </>
-    );
-  }
+							return (
+								<Message
+									message={message}
+									isMyMessage={isMyMessage}
+									msgClasses={msgClasses}
+									key={message.uuid}
+								/>
+							);
+						})}
+					</div>
+				</div>
+				<RoomInput chatSocket={chatSocket} />
+			</>
+		);
+	}
 }
