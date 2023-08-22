c8015db0db046ae58afb3ab63dc5b327b52321a6
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-22 15:23:40 +0200

    Better text ellipsing.

--- frontend/src/pages/UserProfile/UserProfile/Profile/MatchHistory/MatchHistoryCard.tsx
+++ frontend/src/pages/UserProfile/UserProfile/Profile/MatchHistory/MatchHistoryCard.tsx
@@ -19,11 +19,11 @@ interface MatchHistoryCardProps {
 export function MatchHistoryCard(props: MatchHistoryCardProps) {
 	return (
 		<div>
-			<div className="history-user-name">@{props.me.username}</div>
+			<div className="history-user-name ellipsed-txt">@{props.me.username}</div>
 			<div className={props.result === "Victory" ? "history-txt-win" : "history-txt-loose"}>{props.me.score}</div>
 			<img src={props.result === "Victory" ? SwordsIcon : SkullIcon}/>
 			<div className={props.result === "Defeat" ? "history-txt-win" : "history-txt-loose"}>{props.opponent.score}</div>
-			<div className="history-user-name">@{props.opponent.username}</div>
+			<div className="history-user-name ellipsed-txt">@{props.opponent.username}</div>
 		</div>
 	);
 }
