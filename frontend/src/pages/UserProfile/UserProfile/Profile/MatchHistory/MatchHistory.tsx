6c1801e5b4b7d3fc7ea0d15ce20e412d3277af46
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-22 13:56:16 +0200

    History card progress.

--- frontend/src/pages/UserProfile/UserProfile/Profile/MatchHistory/MatchHistory.tsx
+++ frontend/src/pages/UserProfile/UserProfile/Profile/MatchHistory/MatchHistory.tsx
@@ -16,10 +16,12 @@ interface UserMatchHistory {
 	user1_username: string;
 	user2_username: string;
 }
+
 interface MatchHistoryProps {
 	userId: number;
 	username: string;
 }
+
 export function MatchHistory(props: MatchHistoryProps) {
 	const [history, setHistory] = React.useState<UserMatchHistory[]>([]);
 
@@ -41,46 +43,48 @@ export function MatchHistory(props: MatchHistoryProps) {
 		<div id="history-card">
 			<div className="window-title card-title">History</div>
 			{history === undefined ? (
-				<>loading</>
+				<div id="history-values">loading</div>
 			) : history.length === 0 ? (
-				<>no matches</>
+				<div id="history-values">no matches</div>
 			) : (
-				history.map((match, index) => {
-					let opponent: UserScore;
-					let me: UserScore;
-					let matchResult: MatchResult;
-					let userScore_1: UserScore = {
-						username: match.user1_username,
-						score: match.points_user1,
-						user_id: match.user1_id,
-					};
-					let userScore_2: UserScore = {
-						username: match.user2_username,
-						score: match.points_user2,
-						user_id: match.user2_id,
-					};
-					if (match.user1_id === props.userId) {
-						me = userScore_1;
-						opponent = userScore_2;
-						if (match.points_user1 > match.points_user2)
-							matchResult = MatchResult.victory;
-						else matchResult = MatchResult.defeat;
-					} else {
-						opponent = userScore_1;
-						me = userScore_2;
-						if (match.points_user1 > match.points_user2)
-							matchResult = MatchResult.defeat;
-						else matchResult = MatchResult.victory;
-					}
-					return (
-						<MatchHistoryCard
-							result={matchResult}
-							me={me}
-							opponent={opponent}
-							key={index}
-						/>
-					);
-				})
+				<div id="history-values">
+					{history.map((match, index) => {
+						let opponent: UserScore;
+						let me: UserScore;
+						let matchResult: MatchResult;
+						let userScore_1: UserScore = {
+							username: match.user1_username,
+							score: match.points_user1,
+							user_id: match.user1_id,
+						};
+						let userScore_2: UserScore = {
+							username: match.user2_username,
+							score: match.points_user2,
+							user_id: match.user2_id,
+						};
+						if (match.user1_id === props.userId) {
+							me = userScore_1;
+							opponent = userScore_2;
+							if (match.points_user1 > match.points_user2)
+								matchResult = MatchResult.victory;
+							else matchResult = MatchResult.defeat;
+						} else {
+							opponent = userScore_1;
+							me = userScore_2;
+							if (match.points_user1 > match.points_user2)
+								matchResult = MatchResult.defeat;
+							else matchResult = MatchResult.victory;
+						}
+						return (
+							<MatchHistoryCard
+								result={matchResult}
+								me={me}
+								opponent={opponent}
+								key={index}
+							/>
+						);
+					})}
+				</div>
 			)}
 		</div>
 	);
--- frontend/src/pages/UserProfile/UserProfile/Profile/MatchHistory/MatchHistoryCard.tsx
+++ frontend/src/pages/UserProfile/UserProfile/Profile/MatchHistory/MatchHistoryCard.tsx
@@ -1,31 +1,27 @@
 import React from "react";
 import { UserScore } from "./MatchHistory";
+import "../Profile.css";
 
 export enum MatchResult {
-  victory = "Victory",
-  defeat = "Defeat",
+	victory = "Victory",
+	defeat = "Defeat",
 }
-interface MatchHistoryCardProps {
-  result: MatchResult;
-  opponent: UserScore;
-  me: UserScore;
-  key: number;
-}
-export function MatchHistoryCard(props: MatchHistoryCardProps) {
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        gap: "15px",
-      }}
-    >
-      <div>@{props.me.username}</div>
 
-      <div>{props.me.score}</div>
-      <div>{props.result}</div>
-      <div>{props.opponent.score}</div>
-      <div>@{props.opponent.username}</div>
-    </div>
-  );
+interface MatchHistoryCardProps {
+	result: MatchResult;
+	opponent: UserScore;
+	me: UserScore;
+	key: number;
+}
+
+export function MatchHistoryCard(props: MatchHistoryCardProps) {
+	return (
+		<div>
+			<div>@{props.me.username}</div>
+			<div>{props.me.score}</div>
+			<div>{props.result}</div>
+			<div>{props.opponent.score}</div>
+			<div>@{props.opponent.username}</div>
+		</div>
+	);
 }
