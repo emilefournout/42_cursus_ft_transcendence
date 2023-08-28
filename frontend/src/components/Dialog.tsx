6b95221a0001044b4a7d5efaa556642647421d07
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-28 17:57:16 +0200

    Dialog styling.

--- frontend/src/components/Dialog.tsx
+++ frontend/src/components/Dialog.tsx
@@ -1,17 +1,23 @@
 import React from "react";
+import "./Dialog.css";
 
 interface DialogProps {
-  dialog: string | undefined;
-  setDialog: (dialog: string | undefined) => void;
+	dialog: string | undefined;
+	setDialog: (dialog: string | undefined) => void;
 }
+
 export function Dialog(props: DialogProps) {
-  const closeDialog = () => props.setDialog(undefined);
-  return (
-    <dialog open={!!props.dialog}>
-      {props.dialog}
-      <div>
-        <button onClick={closeDialog}>close</button>
-      </div>
-    </dialog>
-  );
+	const closeDialog = () => props.setDialog(undefined);
+	return (
+		<>
+			{props.dialog ? (
+				<dialog className="dialog-window wrapper-col" open={!!props.dialog}>
+					{props.dialog}
+					<button onClick={closeDialog}>Close</button>
+				</dialog>
+			) : (
+				<></>
+			)}
+		</>
+	);
 }
