aa136563d726c7dd09c9f534bcda12922665f8af
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-28 18:37:07 +0200

    Default avatar img.

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -2,92 +2,95 @@ import React, { useCallback, useEffect, useState } from "react";
 import iconVect from "../common/change-icon.svg";
 
 type AvatarProps = {
-  url?: string;
-  size: string;
-  upload: boolean;
-  setImg?: React.Dispatch<React.SetStateAction<File | undefined>>
-  download: boolean;
+	url?: string;
+	size: string;
+	upload: boolean;
+	setImg?: React.Dispatch<React.SetStateAction<File | undefined>>
+	download: boolean;
 }
 
 export function Avatar(props: AvatarProps) {
-  const [img, setImg] = useState<string>();
-  const downloadAvatar = useCallback(async () => {
-    const avatarUrl: string | null =
-      props.url !== undefined
-        ? props.url
-        : await fetch(`${process.env.REACT_APP_BACKEND}/user/me`, {
-            method: "GET",
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-            },
-          })
-            .then((response) => {
-              if (response.ok) return response.json();
-              else throw new Error("Error fetching avatar");
-            })
-            .then((data) => data.avatar);
+	const [img, setImg] = useState<string>();
+	const downloadAvatar = useCallback(async () => {
+		const avatarUrl: string | null =
+			props.url !== undefined
+				? props.url
+				: await fetch(`${process.env.REACT_APP_BACKEND}/user/me`, {
+						method: "GET",
+						headers: {
+							Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+						},
+					})
+						.then((response) => {
+							if (response.ok) return response.json();
+							else throw new Error("Error fetching avatar");
+						})
+						.then((data) => data.avatar);
 
-    fetch(`${process.env.REACT_APP_BACKEND}/profile/${avatarUrl}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-    })
-      .then((response) => {
-        if (!response.ok) throw new Error("Error fetching avatar");
-        return response.blob();
-      })
-      .then((blob) => {
-        const url = URL.createObjectURL(blob);
-        setImg(url);
-      });
-  }, [props.url]);
+		fetch(`${process.env.REACT_APP_BACKEND}/profile/${avatarUrl}`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+			},
+		})
+			.then((response) => {
+				if (!response.ok) throw new Error("Error fetching avatar");
+				return response.blob();
+			})
+			.then((blob) => {
+				const url = URL.createObjectURL(blob);
+				setImg(url);
+			});
+	}, [props.url]);
 
-  useEffect(() => {
-    if (props.download) {
-      downloadAvatar().catch((e) => console.log("Error when downloading the avatar"));
-      return () => {};
-    }
-  }, [downloadAvatar]);
+	useEffect(() => {
+		if (props.download) {
+			downloadAvatar().catch((e) => console.log("Error when downloading the avatar"));
+			return () => {};
+		}
+	}, [downloadAvatar]);
 
-  function saveImage(event: React.ChangeEvent<HTMLInputElement>) {
-    if (event.target.files?.length) {
-      setImg(URL.createObjectURL(event.target.files[0]));
-      if (props.setImg) props.setImg(event.target.files[0])
-    }
-  }
+	function saveImage(event: React.ChangeEvent<HTMLInputElement>) {
+		if (event.target.files?.length) {
+			setImg(URL.createObjectURL(event.target.files[0]));
+			if (props.setImg) props.setImg(event.target.files[0])
+		}
+	}
 
-  return (
-    <>
-      {props.upload ? (
-        <div
-          className="wrapper-img"
-          style={{ "--img-size": props.size } as React.CSSProperties}
-        >
-          <label htmlFor="upload">
-            <img src={img} className="user-avatar" alt="Avatar of the user" />
-            <img
-              id="change-img"
-              src={iconVect}
-              alt="Selecting the avatar icon"
-            />
-            <input
-              type="file"
-              id="upload"
-              name="avatar"
-              style={{ display: "none" }}
-              onChange={saveImage}
-            />
-          </label>
-        </div>
-      ) : (
-        <img
-          src={img}
-          style={{ "--img-size": props.size } as React.CSSProperties}
-          className="user-avatar user-avatar-no-edit"
-          alt="Avatar of the user"
-        />
-      )}
-    </>
-  );
+	if (img === undefined)
+		setImg("https://img.freepik.com/premium-vector/account-icon-user-icon-vector-graphics_292645-552.jpg");
+
+	return (
+		<>
+			{props.upload ? (
+				<div
+					className="wrapper-img"
+					style={{ "--img-size": props.size } as React.CSSProperties}
+				>
+					<label htmlFor="upload">
+						<img src={img} className="user-avatar" alt="Avatar of the user" />
+						<img
+							id="change-img"
+							src={iconVect}
+							alt="Selecting the avatar icon"
+						/>
+						<input
+							type="file"
+							id="upload"
+							name="avatar"
+							style={{ display: "none" }}
+							onChange={saveImage}
+						/>
+					</label>
+				</div>
+			) : (
+				<img
+					src={img}
+					style={{ "--img-size": props.size } as React.CSSProperties}
+					className="user-avatar user-avatar-no-edit"
+					alt="Avatar of the user"
+				/>
+			)}
+		</>
+	);
 }
--- frontend/src/pages/Welcome/Welcome.tsx
+++ frontend/src/pages/Welcome/Welcome.tsx
@@ -6,80 +6,80 @@ import Cookies from "js-cookie";
 import { Avatar } from "../../components/Avatar";
 
 export function Welcome() {
-  const [username, setUsername] = useState("");
-  const [image, setImage] = useState<File>();
-  const navigate = useNavigate();
-  const navigateError = useNavigate();
-  // Guest mode for no 42-students
-  const [searchParams, setSearchParams] = useSearchParams();
+	const [username, setUsername] = useState("");
+	const [image, setImage] = useState<File>();
+	const navigate = useNavigate();
+	const navigateError = useNavigate();
+	// Guest mode for no 42-students
+	const [searchParams, setSearchParams] = useSearchParams();
 
-  useEffect(() => {
-    if (searchParams.get("guest")) return;
-    if (!Cookies.get("42token") || Cookies.get("42token") == "j:null") {
-      Cookies.remove("42token");
-      navigateError("/cookieError");
-    }
-  });
+	useEffect(() => {
+		if (searchParams.get("guest")) return;
+		if (!Cookies.get("42token") || Cookies.get("42token") == "j:null") {
+			Cookies.remove("42token");
+			navigateError("/cookieError");
+		}
+	});
 
-  function register() {
-    const token: string | undefined = searchParams.get("guest")
-      ? "guest"
-      : Cookies.get("42token");
-    const formData = new FormData();
-    formData.append("username", username);
-    formData.append("image", image as File);
-    fetch(`${process.env.REACT_APP_BACKEND}/auth/register`, {
-      method: "POST",
-      body: formData,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((response: Response) => {
-        if (response.ok) return response.json();
-        else throw new Error("Error registering");
-      })
-      .then((data) => {
-        if (data.access_token) {
-          localStorage.setItem("access_token", data.access_token);
-          localStorage.setItem("username", data.username);
-          navigate("/");
-        } else throw new Error();
-      })
-      .catch((error) => console.log(error));
-  }
+	function register() {
+		const token: string | undefined = searchParams.get("guest")
+			? "guest"
+			: Cookies.get("42token");
+		const formData = new FormData();
+		formData.append("username", username);
+		formData.append("image", image as File);
+		fetch(`${process.env.REACT_APP_BACKEND}/auth/register`, {
+			method: "POST",
+			body: formData,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+			.then((response: Response) => {
+				if (response.ok) return response.json();
+				else throw new Error("Error registering");
+			})
+			.then((data) => {
+				if (data.access_token) {
+					localStorage.setItem("access_token", data.access_token);
+					localStorage.setItem("username", data.username);
+					navigate("/");
+				} else throw new Error();
+			})
+			.catch((error) => console.log(error));
+	}
 
-  return (
-    <div className="window-module">
-      <div className="window-top-bar">
-        <div className="window-title">Welcome</div>
-        <div className="wrapper-col window-overtext">
-          <span className="txt txt-shadow-top">
-            A bit of setup before we begin:
-          </span>
-          <div className="underline wp-underline"></div>
-        </div>
-      </div>
-      <div className="window-body-centered">
-        <div className="wrapper-welcome-grid">
-          <Avatar size="128px" upload={true} download={false} setImg={setImage}/>
-          <input
-            id="wp-username-input"
-            className="wp-responsive-txt"
-            type="text"
-            placeholder="User Name"
-            onChange={(event) => setUsername(event.target.value)}
-            required
-          />
-          <button
-            id="wp-submit"
-            className="btn btn-bottom-right wp-responsive-txt"
-            onClick={register}
-          >
-            Done!
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="window-module">
+			<div className="window-top-bar">
+				<div className="window-title">Welcome</div>
+				<div className="wrapper-col window-overtext">
+					<span className="txt txt-shadow-top">
+						A bit of setup before we begin:
+					</span>
+					<div className="underline wp-underline"></div>
+				</div>
+			</div>
+			<div className="window-body-centered">
+				<div className="wrapper-welcome-grid">
+					<Avatar size="128px" upload={true} download={false} setImg={setImage}/>
+					<input
+						id="wp-username-input"
+						className="wp-responsive-txt"
+						type="text"
+						placeholder="User Name"
+						onChange={(event) => setUsername(event.target.value)}
+						required
+					/>
+					<button
+						id="wp-submit"
+						className="btn btn-bottom-right wp-responsive-txt"
+						onClick={register}
+					>
+						Done!
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
