1f591a0a59e5b32762acea03898231671f23ff39
José Manuel <josesanc@student.42malaga.com>
2023-08-29 13:50:44 +0200

    🗃️ Changed Tic Tac Toe achievement

--- backend/sql/insertions.sql
+++ backend/sql/insertions.sql
@@ -2,6 +2,6 @@ INSERT INTO "Achievement" (name, description)
 VALUES
     ('First Win', 'Win your first game'),
     ('KO', 'Win a game without any goal to be scored'),
-    ('Tic Tac Toe', 'Win three games in a row');
+    ('eSport trainee', 'Win ten games');
 
 INSERT INTO "User" (username, intraname) VALUES ('User Deleted', 'User Deleted')
