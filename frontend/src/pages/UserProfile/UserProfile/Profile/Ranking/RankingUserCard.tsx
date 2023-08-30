b6c4d8458b563f426b993fec42cea3a06ee8a468
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-30 12:27:10 +0200

    Change name page fixes.

--- frontend/src/pages/UserProfile/UserProfile/Profile/Ranking/RankingUserCard.tsx
+++ frontend/src/pages/UserProfile/UserProfile/Profile/Ranking/RankingUserCard.tsx
@@ -4,19 +4,20 @@ import { useNavigate } from "react-router-dom";
 import { User } from "../../../../Board/Board";
 
 interface RankingUserCardProps {
-  user: User;
-  position: number;
-  key: string;
+	user: User;
+	position: number;
+	key: string;
 }
+
 export function RankingUserCard(props: RankingUserCardProps) {
-  const navigate = useNavigate();
-  return (
-    <div style={{ display: "flex", flexDirection: "row", gap: "15px" }}>
-      <Avatar url={props.user.avatar} size="64px" upload={false} download={true}/>
-      <div>#{props.position}</div>
-      <div>{props.user.username}</div>
-      <div>win:{props.user.wins}</div>
-      <div>lose:{props.user.loses}</div>
-    </div>
-  );
+	const navigate = useNavigate();
+	return (
+		<div style={{ display: "flex", flexDirection: "row", gap: "15px" }}>
+			<Avatar url={props.user.avatar} size="64px" upload={false} download={true}/>
+			<div>#{props.position}</div>
+			<div>{props.user.username}</div>
+			<div>win:{props.user.wins}</div>
+			<div>lose:{props.user.loses}</div>
+		</div>
+	);
 }
