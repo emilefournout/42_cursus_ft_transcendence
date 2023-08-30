18c195d48ae81e0bf6ec3c444cd69ab46adfe2b6
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-30 16:18:44 +0200

    Game page styling.

--- frontend/src/pages/Game/GameHomePage/GameHomePage.tsx
+++ frontend/src/pages/Game/GameHomePage/GameHomePage.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import SEO from "../../../components/Seo";
 import { Link } from "react-router-dom";
+import "../Game.css";
 
 export function GameHomePage() {
-  return (
-    <>
-      <SEO
-        title="Pong - Game"
-        description="Start a game with someone from the Internet with matchmaking or invite one of your friends."
-      />
-      <Link to="/board/game/newGame">
-        <button className="btn btn-fixed-height">Create a new game</button>
-      </Link>
-      <Link to="/board/game/matchmaking">
-        <button className="btn btn-fixed-height">Matchmaking</button>
-      </Link>
-      {/*separator or*/}
-      {/*Invite Friend Form*/}
-    </>
-  );
+	return (
+		<>
+			<SEO
+				title="Pong - Game"
+				description="Start a game with someone from the Internet with matchmaking or invite one of your friends."
+			/>
+			<Link className="btn game-creation-btn" to="/board/game/matchmaking">
+				Matchmaking
+			</Link>
+			<div className="wrapper-row txt game-creation-divider">Or</div>
+			<Link className="btn btn-bottom game-creation-btn" to="/board/game/newGame">
+				Create a new game
+			</Link>
+			{/*separator or*/}
+			{/*Invite Friend Form*/}
+		</>
+	);
 }
