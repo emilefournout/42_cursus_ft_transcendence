4b80ca64cef897cbc96c0318aafc8af253b8af8c
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-30 17:26:35 +0200

    Creation page and scrolling fixed.

--- frontend/src/pages/Game/GameCreateGamePage/GameCreateGamePage.tsx
+++ frontend/src/pages/Game/GameCreateGamePage/GameCreateGamePage.tsx
@@ -3,43 +3,43 @@ import SEO from "../../../components/Seo";
 import "./GameCreateGamePage.css";
 
 export function GameCreateGamePage() {
-  const [numberValue, setValue] = useState(10)
-  const MAX_GOALS = 25;
-  const MIN_GOALS = 5;
-  return (
-    <>
-      <SEO
-        title="Pong - Create a game"
-        description="Customize your game and start playing."
-      />
-      <div className="wrapper-matchmaking">
-        <p className="matchmaking-scaling">Create a new game for you</p>
-        <fieldset>
-          <label htmlFor="numberOfGoals" >Number of Goals (5 - 25)</label>
-          <input id="numberOfGoals" type="number" min={MIN_GOALS} max={MAX_GOALS} value={numberValue} onChange={checkValueLimits}/>
-          <label htmlFor="speedControl" >SpeedControl</label>
-          <input id="speedControl" type="range" min={0.75} max={1.25} step={0.01} defaultValue={1}/>
-          <label htmlFor="powerUp">PowerUps</label>
-          <div className="wrapper-col-centered">
-            <input id="powerUp" type="checkbox"/>
-          </div>
-        </fieldset>
-        <button className="btn btn-fixed-height">
-          Create (TODO)
-        </button>
-      </div>
-    </>
-  );
+	const [numberValue, setValue] = useState(10)
+	const MAX_GOALS = 25;
+	const MIN_GOALS = 5;
+	return (
+		<>
+			<SEO
+				title="Pong - Create a game"
+				description="Customize your game and start playing."
+			/>
+			<div className="wrapper-matchmaking">
+				<p className="create-game-title">Create a new game!</p>
+				<fieldset>
+					<label htmlFor="numberOfGoals" >Number of Goals (5 - 25)</label>
+					<input id="numberOfGoals" type="number" min={MIN_GOALS} max={MAX_GOALS} value={numberValue} onChange={checkValueLimits}/>
+					<label htmlFor="speedControl" >SpeedControl</label>
+					<input id="speedControl" type="range" min={0.75} max={1.25} step={0.01} defaultValue={1}/>
+					<label htmlFor="powerUp">PowerUps</label>
+					<div className="wrapper-col-centered">
+						<input id="powerUp" type="checkbox"/>
+					</div>
+				</fieldset>
+				<button className="btn game-creation-btn create-game-btn btn-bottom">
+					Create (TODO)
+				</button>
+			</div>
+		</>
+	);
 
-  function checkValueLimits(event: React.ChangeEvent<HTMLInputElement>) {
-    const value: number = parseInt(event.target.value);
-    if (Number.isInteger(value)){
-      if (value > MAX_GOALS)
-        setValue(MAX_GOALS);
-      else if (value < MIN_GOALS)
-        setValue(MIN_GOALS);
-      else
-        setValue(value);
-    }
-  };
+	function checkValueLimits(event: React.ChangeEvent<HTMLInputElement>) {
+		const value: number = parseInt(event.target.value);
+		if (Number.isInteger(value)){
+			if (value > MAX_GOALS)
+				setValue(MAX_GOALS);
+			else if (value < MIN_GOALS)
+				setValue(MIN_GOALS);
+			else
+				setValue(value);
+		}
+	};
 }
--- frontend/src/pages/Game/GamePlayPage/GamePlayPage.tsx
+++ frontend/src/pages/Game/GamePlayPage/GamePlayPage.tsx
@@ -4,101 +4,101 @@ import { GameSocket } from "../../../services/socket";
 import { useNavigate, useParams } from "react-router-dom";
 
 enum GameExistState {
-  Waiting,
-  NotFound,
-  Play,
+	Waiting,
+	NotFound,
+	Play,
 }
 
 export function GamePlayPage() {
-  const [gameExistState, setGameExistState] = useState(GameExistState.Waiting);
-  const { id } = useParams();
-  const gameSocket = GameSocket.getInstance().socket;
-  const navigate = useNavigate();
-  const boardState: GameCanvasProps = {
-    width: 600,
-    height: 350,
-    padWidth: 10,
-    padHeight: 60,
-    ballRadius: 8,
-    leftPad: 100,
-    rightPad: 150,
-    ballX: 100,
-    ballY: 150,
-  };
+	const [gameExistState, setGameExistState] = useState(GameExistState.Waiting);
+	const { id } = useParams();
+	const gameSocket = GameSocket.getInstance().socket;
+	const navigate = useNavigate();
+	const boardState: GameCanvasProps = {
+		width: 600,
+		height: 350,
+		padWidth: 10,
+		padHeight: 60,
+		ballRadius: 8,
+		leftPad: 100,
+		rightPad: 150,
+		ballX: 100,
+		ballY: 150,
+	};
 
-  const [state, updateGameState] = useState(boardState);
-  const [player1Score, updatePlayer1Score] = useState(0);
-  const [player2Score, updatePlayer2Score] = useState(0);
+	const [state, updateGameState] = useState(boardState);
+	const [player1Score, updatePlayer1Score] = useState(0);
+	const [player2Score, updatePlayer2Score] = useState(0);
 
-  const [showModal, setShowModal] = useState(null);
+	const [showModal, setShowModal] = useState(null);
 
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_BACKEND}/game/info/${id}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          setGameExistState(GameExistState.NotFound);
-        }
-      })
-      .then((body) => {
-        if (body && body.status !== "PLAYING")
-          setGameExistState(GameExistState.NotFound);
-        else {
-          var path = window.location.pathname;
-          path = path.substring(path.lastIndexOf("/") + 1);
-          console.log("Path is " + path);
+	useEffect(() => {
+		fetch(`${process.env.REACT_APP_BACKEND}/game/info/${id}`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+			},
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					setGameExistState(GameExistState.NotFound);
+				}
+			})
+			.then((body) => {
+				if (body && body.status !== "PLAYING")
+					setGameExistState(GameExistState.NotFound);
+				else {
+					var path = window.location.pathname;
+					path = path.substring(path.lastIndexOf("/") + 1);
+					console.log("Path is " + path);
 
-          gameSocket.emit("join_active_room", path);
-          setGameExistState(GameExistState.Play);
-        }
-      });
-  }, [id, gameSocket]);
+					gameSocket.emit("join_active_room", path);
+					setGameExistState(GameExistState.Play);
+				}
+			});
+	}, [id, gameSocket]);
 
-  useEffect(() => {
-    gameSocket.off("update");
-    gameSocket.on("update", (data: any) => {
-      updateGameState(data);
-      updatePlayer1Score(data.player1Score);
-      updatePlayer2Score(data.player2Score);
-    });
-  }, [gameSocket]);
+	useEffect(() => {
+		gameSocket.off("update");
+		gameSocket.on("update", (data: any) => {
+			updateGameState(data);
+			updatePlayer1Score(data.player1Score);
+			updatePlayer2Score(data.player2Score);
+		});
+	}, [gameSocket]);
 
-  useEffect(() => {
-    gameSocket.off("end");
-    gameSocket.on("end", (data: any) => {
-      setShowModal(data);
-    });
-  }, [gameSocket]);
+	useEffect(() => {
+		gameSocket.off("end");
+		gameSocket.on("end", (data: any) => {
+			setShowModal(data);
+		});
+	}, [gameSocket]);
 
-  return (
-    <>
-      {gameExistState === GameExistState.Waiting ? (
-        <div>Waiting</div>
-      ) : gameExistState === GameExistState.NotFound ? (
-        <div>Not Found</div>
-      ) : (
-        <>
-          <h1 className="title">Game</h1>
-          <div className="title">
-            {player1Score} - {player2Score}
-          </div>
-          <GameCanvas {...state} />
-          {showModal !== null && (
-            <div className="modal">
-              <div className="modal-content">
-                <p>{showModal} wins</p>
-                <button onClick={() => navigate("/board")}>HOME</button>
-              </div>
-            </div>
-          )}
-        </>
-      )}
-    </>
-  );
+	return (
+		<>
+			{gameExistState === GameExistState.Waiting ? (
+				<div>Waiting</div>
+			) : gameExistState === GameExistState.NotFound ? (
+				<div>Not Found</div>
+			) : (
+				<>
+					<h1 className="title">Game</h1>
+					<div className="title">
+						{player1Score} - {player2Score}
+					</div>
+					<GameCanvas {...state} />
+					{showModal !== null && (
+						<div className="modal">
+							<div className="modal-content">
+								<p>{showModal} wins</p>
+								<button onClick={() => navigate("/board")}>HOME</button>
+							</div>
+						</div>
+					)}
+				</>
+			)}
+		</>
+	);
 }
