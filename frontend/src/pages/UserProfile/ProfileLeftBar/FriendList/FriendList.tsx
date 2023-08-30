86d69fc0cde4f6141be522d63e0f44d396a6cd18
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-08-30 13:40:03 +0200

    Pending friend card fixes.

--- frontend/src/pages/UserProfile/ProfileLeftBar/FriendList/FriendList.tsx
+++ frontend/src/pages/UserProfile/ProfileLeftBar/FriendList/FriendList.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {
-  AcceptedFriendCard,
-  FriendCardProps,
+	AcceptedFriendCard,
+	FriendCardProps,
 } from "./FriendCard/AcceptedFriendCard/AcceptedFriendCard";
 import { ProfilePageContext, RequestType } from "../../UserProfilePage";
 import { ProfileBarContext } from "../ProfileLeftBar";
@@ -12,69 +12,69 @@ import "./FriendList.css";
 import FriendLessImg from "./NoFriends.png";
 
 export function FriendList() {
-  const profileBarContext = React.useContext(ProfileBarContext);
-  const profilePageContext = React.useContext(ProfilePageContext);
-  const isEnabled = () =>
-    profileBarContext.requestsType === RequestType.enabled;
-  const isEnabledUndefined = () =>
-    isEnabled() && profilePageContext.acceptedFriends === undefined;
+	const profileBarContext = React.useContext(ProfileBarContext);
+	const profilePageContext = React.useContext(ProfilePageContext);
+	const isEnabled = () =>
+		profileBarContext.requestsType === RequestType.enabled;
+	const isEnabledUndefined = () =>
+		isEnabled() && profilePageContext.acceptedFriends === undefined;
 
-  const isPending = () =>
-    profileBarContext.requestsType === RequestType.pending;
-  const isPendingUndefined = () =>
-    isPending() &&
-    (profilePageContext.pendingFriends === undefined ||
-      profilePageContext.receivedFriends === undefined);
+	const isPending = () =>
+		profileBarContext.requestsType === RequestType.pending;
+	const isPendingUndefined = () =>
+		isPending() &&
+		(profilePageContext.pendingFriends === undefined ||
+			profilePageContext.receivedFriends === undefined);
 
-  const isEnabledEmpty = () =>
-    isEnabled() && profilePageContext.acceptedFriends!.length === 0;
+	const isEnabledEmpty = () =>
+		isEnabled() && profilePageContext.acceptedFriends!.length === 0;
 
-  const isPendingEmpty = () =>
-    isPending() &&
-    profilePageContext.pendingFriends!.length === 0 &&
-    profilePageContext.receivedFriends!.length === 0;
+	const isPendingEmpty = () =>
+		isPending() &&
+		profilePageContext.pendingFriends!.length === 0 &&
+		profilePageContext.receivedFriends!.length === 0;
 
-  const buildFriendList = (
-    FriendCard: (props: FriendCardProps) => React.JSX.Element,
-    friends: User[]
-  ) => {
-    return (
-      <>
-        {friends.map((userInfo: User, index) => {
-          return <div className="friendcard-container" key={index}>{<FriendCard userInfo={userInfo} />}</div>;
-        })}
-      </>
-    );
-  };
+	const buildFriendList = (
+		FriendCard: (props: FriendCardProps) => React.JSX.Element,
+		friends: User[]
+	) => {
+		return (
+			<>
+				{friends.map((userInfo: User, index) => {
+					return <div className="friendcard-container" key={index}>{<FriendCard userInfo={userInfo} />}</div>;
+				})}
+			</>
+		);
+	};
 
-  if (isEnabledUndefined() || isPendingUndefined()) {
-    return <div className="friendlist-wrapper">Loading...</div>;
-  } else if (isEnabledEmpty()) {
-    return (
-      <div className="friendlist-wrapper">
-        Friendless
-        <img src={FriendLessImg} />
-      </div>
-    );
-  } else if (isPendingEmpty()) {
-    return <div className="friendlist-wrapper">No Pending Requests!</div>;
-  } else if (isEnabled()) {
-    return buildFriendList(
-      AcceptedFriendCard,
-      profilePageContext.acceptedFriends!
-    );
-  } else {
-    return (
-      <>
-        {buildFriendList(
-          ReceivedFriendCard,
-          profilePageContext.receivedFriends!
-        )}
-        {buildFriendList(
-          PendingFriendCard,
-          profilePageContext.pendingFriends!
-        )}
-      </>
-    );
-  }
+	if (isEnabledUndefined() || isPendingUndefined()) {
+		return <div className="friendlist-wrapper">Loading...</div>;
+	} else if (isEnabledEmpty()) {
+		return (
+			<div className="friendlist-wrapper">
+				Friendless
+				<img src={FriendLessImg} />
+			</div>
+		);
+	} else if (isPendingEmpty()) {
+		return <div className="friendlist-wrapper">No Pending Requests!</div>;
+	} else if (isEnabled()) {
+		return buildFriendList(
+			AcceptedFriendCard,
+			profilePageContext.acceptedFriends!
+		);
+	} else {
+		return (
+			<div className="friendlist-wrapper">
+				{buildFriendList(
+					ReceivedFriendCard,
+					profilePageContext.receivedFriends!
+				)}
+				{buildFriendList(
+					PendingFriendCard,
+					profilePageContext.pendingFriends!
+				)}
+			</div>
+		);
+	}
 }
