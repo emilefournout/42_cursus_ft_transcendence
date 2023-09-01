e482c45186dad9ec91502eb95281345610278ca8
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-09-01 13:20:32 +0200

    Room params beginnings. Input.

--- frontend/src/pages/Chat/Room/RoomParam/RoomParam.tsx
+++ frontend/src/pages/Chat/Room/RoomParam/RoomParam.tsx
@@ -6,24 +6,23 @@ import { ChatMembers } from "./ChatMembers/ChatMembers";
 import { RoomContextArgs } from "../Room";
 import { UpdateVisibilityButtons } from "./UpdateVisibilityButtons";
 import { BoardContext } from "../../../Board/Board";
+
 export function RoomParam() {
-  const roomContextArgs = useOutletContext<RoomContextArgs>();
-  const boardContext = useContext(BoardContext);
-  const me = roomContextArgs.chat.members?.find(
-    (member) => member.userId === boardContext?.me.id
-  );
+	const roomContextArgs = useOutletContext<RoomContextArgs>();
+	const boardContext = useContext(BoardContext);
+	const me = roomContextArgs.chat.members?.find(
+		(member) => member.userId === boardContext?.me.id
+	);
 
-  const isOwner: boolean | undefined = me?.owner === true;
-  const isAdmin: boolean | undefined = me?.administrator === true;
+	const isOwner: boolean | undefined = me?.owner === true;
+	const isAdmin: boolean | undefined = me?.administrator === true;
 
-  if (isAdmin === undefined || isOwner === undefined) return <></>;
-  return (
-    <div className="room-param">
-      {isOwner && <UpdateVisibilityButtons />}
-      <div style={{ padding: "10px" }}>
-        <AddUser />
-        <ChatMembers isManager={isOwner || isAdmin} />
-      </div>
-    </div>
-  );
+	if (isAdmin === undefined || isOwner === undefined) return <></>;
+	return (
+		<div className="room-param">
+			<AddUser />
+			{/* {isOwner && <UpdateVisibilityButtons />}
+			  * <ChatMembers isManager={isOwner || isAdmin} /> */}
+		</div>
+	);
 }
