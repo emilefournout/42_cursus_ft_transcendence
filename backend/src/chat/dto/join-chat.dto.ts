ce22cd39acd152060553ddc6b02a27d077eb937f
José Manuel <josesanc@student.42malaga.com>
2023-09-01 16:41:26 +0200

    ✨ Allowing user to join protected chats

---
+++ backend/src/chat/dto/join-chat.dto.ts
@@ -0,0 +1,12 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class JoinChatDto {
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
+    @ApiPropertyOptional({
+        type: String
+    })
+    password?: string
+}
