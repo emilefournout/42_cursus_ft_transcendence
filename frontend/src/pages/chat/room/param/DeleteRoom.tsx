eb4359c0965a5ea22663bc65d4533097d0878082
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-09-01 19:12:48 +0200

    Params delete styling.

--- frontend/src/pages/chat/room/param/DeleteRoom.tsx
+++ frontend/src/pages/chat/room/param/DeleteRoom.tsx
@@ -3,45 +3,41 @@ import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { ChatPageContext } from "../../Chat";
 
 export function DeleteRoom() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const { id } = useParams();
-  const back_route = location.pathname.replace("delete", "");
-  const chatPageContext = React.useContext(ChatPageContext);
-  const delete_room = () => {
-    fetch(`${process.env.REACT_APP_BACKEND}/chat/${id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          setTimeout(() => {
-            chatPageContext
-              .updateChat()
-              .then(() => {
-                navigate("/board/chats");
-              })
-              .catch((error) => {
-                console.log(error);
-              });
-          }, 500);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+	const navigate = useNavigate();
+	const location = useLocation();
+	const { id } = useParams();
+	const back_route = location.pathname.replace("delete", "");
+	const chatPageContext = React.useContext(ChatPageContext);
+	const delete_room = () => {
+		fetch(`${process.env.REACT_APP_BACKEND}/chat/${id}`, {
+			method: "DELETE",
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+			},
+		})
+			.then((response) => {
+				if (response.ok) {
+					setTimeout(() => {
+						chatPageContext
+							.updateChat()
+							.then(() => {
+								navigate("/board/chats");
+							})
+							.catch((error) => {
+								console.log(error);
+							});
+					}, 500);
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
-  return (
-    <>
-      <div>
-        <button onClick={() => delete_room()}>delete</button>
-      </div>
-      <div>
-        <button onClick={() => navigate(back_route)}>back</button>
-      </div>
-    </>
-  );
+	return (
+		<div className="delete-page-container">
+			<button id="delete-page-del-btn" onClick={() => delete_room()}>delete</button>
+			<button onClick={() => navigate(back_route)}>back</button>
+		</div>
+	);
 }
