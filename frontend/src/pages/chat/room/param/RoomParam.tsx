2b06f3fae5d1ebcc6d49c4d129679d65ef380a89
emile_frn <emile.fournout@gmail.com>
2023-09-02 19:48:50 +0200

    UpdateVisibilityDialog added

--- frontend/src/pages/chat/room/param/RoomParam.tsx
+++ frontend/src/pages/chat/room/param/RoomParam.tsx
@@ -7,43 +7,50 @@ import { RoomContextArgs } from "../Room";
 import { UpdateVisibilityButtons } from "./UpdateVisibilityButtons";
 import { BoardContext } from "../../../board/Board";
 import { MuteDialog } from "./chat_members/MuteDialog";
+import { UpdateVisibilityDialog } from "./dialogs/UpdateVisibilityDialog";
 
 interface MuteDialogContextArgs {
-	mute: (value: number | undefined) => void;
+  mute: (value: number | undefined) => void;
 }
 export const MuteDialogContext = React.createContext(
-	{} as MuteDialogContextArgs
+  {} as MuteDialogContextArgs
 );
 export function RoomParam() {
-	const roomContextArgs = useOutletContext<RoomContextArgs>();
-	const boardContext = useContext(BoardContext);
-	const me = roomContextArgs.chat.members?.find(
-		(member) => member.userId === boardContext?.me.id
-	);
+  const roomContextArgs = useOutletContext<RoomContextArgs>();
+  const boardContext = useContext(BoardContext);
+  const me = roomContextArgs.chat.members?.find(
+    (member) => member.userId === boardContext?.me.id
+  );
+  const [showUpdateDialog, setShowUpdateDialog] = useState<boolean>(false);
+  const isOwner: boolean | undefined = me?.owner === true;
+  const isAdmin: boolean | undefined = me?.administrator === true;
 
-	const isOwner: boolean | undefined = me?.owner === true;
-	const isAdmin: boolean | undefined = me?.administrator === true;
+  const [userIdToMute, setUserIdToMute] = useState<number | undefined>(
+    undefined
+  );
 
-	const [userIdToMute, setUserIdToMute] = useState<number | undefined>(
-		undefined
-	);
-
-	if (isAdmin === undefined || isOwner === undefined) return <></>;
-	return (
-		<>
-			<MuteDialog
-				userIdToMute={userIdToMute}
-				setUserIdToMute={setUserIdToMute}
-			/>
-			<div className="room-param">
-				<AddUser />
-				{isOwner && <UpdateVisibilityButtons />}
-				<MuteDialogContext.Provider
-					value={{ mute: setUserIdToMute } as MuteDialogContextArgs}
-				>
-					<ChatMembers isManager={isOwner || isAdmin} />
-				</MuteDialogContext.Provider>
-			</div>
-		</>
-	);
+  if (isAdmin === undefined || isOwner === undefined) return <></>;
+  return (
+    <>
+      <MuteDialog
+        userIdToMute={userIdToMute}
+        setUserIdToMute={setUserIdToMute}
+      />
+      <UpdateVisibilityDialog
+        showUpdateDialog={showUpdateDialog}
+        setShowUpdateDialog={setShowUpdateDialog}
+      />
+      <div className="room-param">
+        <AddUser />
+        {isOwner && (
+          <UpdateVisibilityButtons updateVisibility={setShowUpdateDialog} />
+        )}
+        <MuteDialogContext.Provider
+          value={{ mute: setUserIdToMute } as MuteDialogContextArgs}
+        >
+          <ChatMembers isManager={isOwner || isAdmin} />
+        </MuteDialogContext.Provider>
+      </div>
+    </>
+  );
 }
--- frontend/src/pages/chat/room/param/UpdateVisibilityButtons.tsx
+++ frontend/src/pages/chat/room/param/UpdateVisibilityButtons.tsx
@@ -4,75 +4,31 @@ import { useNavigate, useOutletContext } from "react-router-dom";
 import { RoomContextArgs } from "../Room";
 import { ChatPageContext } from "../../Chat";
 import { DialogContext } from "../../../root/Root";
+interface UpdateVisibilityButtonsProps {
+  updateVisibility: (open: boolean) => void;
+}
+export function UpdateVisibilityButtons(props: UpdateVisibilityButtonsProps) {
+  const navigate = useNavigate();
+  const roomContextArgs = useOutletContext<RoomContextArgs>();
+  const dialogContext = useContext(DialogContext);
+  const setDialog = dialogContext.setDialog;
+  const chatPageContext = useContext(ChatPageContext);
 
-export function UpdateVisibilityButtons() {
-	const navigate = useNavigate();
-	const roomContextArgs = useOutletContext<RoomContextArgs>();
-	const [updateIsSelected, setUpdateIsSelected] = useState(false);
-	const dialogContext = useContext(DialogContext);
-	const setDialog = dialogContext.setDialog;
-	const chatPageContext = useContext(ChatPageContext);
-
-	const updateVisibility = (visibility: Visibility) => {
-		if (visibility === roomContextArgs.chat.visibility) {
-			setDialog("visibility is already set to " + visibility);
-			return;
-		}
-		if (visibility === Visibility.PROTECTED) {
-			navigate("change-password");
-			return;
-		}
-		fetch(`${process.env.REACT_APP_BACKEND}/chat/${roomContextArgs.chat.id}`, {
-			method: "PATCH",
-			headers: {
-				Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ chatVisibility: Visibility.PUBLIC }),
-		})
-			.then((response) => {
-				if (response.ok) {
-					//roomContextArgs.getChatInfo(roomContextArgs.chat);
-					chatPageContext.updateChat().catch((error) => {
-						console.log(error);
-					});
-					setDialog("visibility updated to " + Visibility.PUBLIC);
-				}
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
-
-	if (roomContextArgs.chat === undefined) {
-		return <></>;
-	} else {
-		return (
-			<>
-				{updateIsSelected ? (
-					<>
-						{roomContextArgs.chat.visibility} Room
-						<div onClick={() => updateVisibility(Visibility.PROTECTED)}>
-							Protected
-						</div>{" "}
-						or{" "}
-						<div onClick={() => updateVisibility(Visibility.PUBLIC)}>
-							Public
-						</div>
-						<button onClick={() => setUpdateIsSelected(false)}>cancel</button>
-					</>
-				) : (
-					<button onClick={() => setUpdateIsSelected(true)}>
-						Update Vis
-					</button>
-				)}
-				{roomContextArgs.chat.visibility === Visibility.PROTECTED && (
-					<button onClick={() => navigate("change-password")}>
-						Change Passwd
-					</button>
-				)}
-				<button id="delete-btn" onClick={() => navigate("delete")}>Delete Room</button>
-			</>
-		);
-	}
+  if (roomContextArgs.chat === undefined) {
+    return <></>;
+  } else {
+    return (
+      <>
+        <button onClick={() => props.updateVisibility(true)}>Update Vis</button>
+        {roomContextArgs.chat.visibility === Visibility.PROTECTED && (
+          <button onClick={() => navigate("change-password")}>
+            Change Passwd
+          </button>
+        )}
+        <button id="delete-btn" onClick={() => navigate("delete")}>
+          Delete Room
+        </button>
+      </>
+    );
+  }
 }
