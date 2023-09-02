48978675f98fc707d7d1322830d8ff30550cf782
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-09-02 15:43:34 +0200

    Achievements progress.

--- frontend/src/pages/user/user_profile/profile/achievements/AchievementCard.tsx
+++ frontend/src/pages/user/user_profile/profile/achievements/AchievementCard.tsx
@@ -1,21 +1,19 @@
 import React from "react";
+import "./../ranking/Ranking.css";
 
 interface AchievementCardProps {
-  title: String;
-  description: String;
-  //image: String;
+	title: String;
+	description: String;
+	//image: String;
 }
+
 export function AchievementCard(props: AchievementCardProps) {
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        gap: "25px",
-      }}
-    >
-      <div>{props.title}</div>
-      <div> {props.description}</div>
-    </div>
-  );
+	return (
+		<div className="ach-card-container">
+			<div className="ach-card-title">{props.title}</div>
+			<div className="ach-card-subtitle">{props.description}</div>
+			<div className="ach-card-back"></div>
+			<img className="ach-card-img" src="" />
+		</div>
+	);
 }
