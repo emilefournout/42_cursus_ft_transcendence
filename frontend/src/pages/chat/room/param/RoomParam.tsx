e160605cd99d5b4f1a72ff0eea78e1db458e848f
emile_frn <emile.fournout@gmail.com>
2023-09-04 13:56:50 +0200

    remove mutedialog

--- frontend/src/pages/chat/room/param/RoomParam.tsx
+++ frontend/src/pages/chat/room/param/RoomParam.tsx
@@ -6,53 +6,53 @@ import { ChatMembers } from "./chat_members/ChatMembers";
 import { RoomContextArgs } from "../Room";
 import { UpdateVisibilityButtons } from "./UpdateVisibilityButtons";
 import { BoardContext } from "../../../board/Board";
-import { MuteDialog } from "./chat_members/MuteDialog";
+import { MuteDialog } from "./dialogs/MuteDialog";
 import { UpdateVisibilityDialog } from "./dialogs/UpdateVisibilityDialog";
 
 interface MuteDialogContextArgs {
-	mute: (value: number | undefined) => void;
+  mute: (value: number | undefined) => void;
 }
 
 export const MuteDialogContext = React.createContext(
-	{} as MuteDialogContextArgs
+  {} as MuteDialogContextArgs
 );
 
 export function RoomParam() {
-	const roomContextArgs = useOutletContext<RoomContextArgs>();
-	const boardContext = useContext(BoardContext);
-	const me = roomContextArgs.chat.members?.find(
-		(member) => member.userId === boardContext?.me.id
-	);
-	const [showUpdateDialog, setShowUpdateDialog] = useState<boolean>(false);
-	const isOwner: boolean | undefined = me?.owner === true;
-	const isAdmin: boolean | undefined = me?.administrator === true;
-	const isManager = isOwner || isAdmin;
-	const [userIdToMute, setUserIdToMute] = useState<number | undefined>(
-		undefined
-	);
+  const roomContextArgs = useOutletContext<RoomContextArgs>();
+  const boardContext = useContext(BoardContext);
+  const me = roomContextArgs.chat.members?.find(
+    (member) => member.userId === boardContext?.me.id
+  );
+  const [showUpdateDialog, setShowUpdateDialog] = useState<boolean>(false);
+  const isOwner: boolean | undefined = me?.owner === true;
+  const isAdmin: boolean | undefined = me?.administrator === true;
+  const isManager = isOwner || isAdmin;
+  const [userIdToMute, setUserIdToMute] = useState<number | undefined>(
+    undefined
+  );
 
-	if (isAdmin === undefined || isOwner === undefined) return <></>;
-	return (
-		<>
-			<MuteDialog
-				userIdToMute={userIdToMute}
-				setUserIdToMute={setUserIdToMute}
-			/>
-			<UpdateVisibilityDialog
-				showUpdateDialog={showUpdateDialog}
-				setShowUpdateDialog={setShowUpdateDialog}
-			/>
-			<div className="room-param">
-				{isManager ? <AddUser /> : <>Members:</>}
-				{isOwner && (
-					<UpdateVisibilityButtons updateVisibility={setShowUpdateDialog} />
-				)}
-				<MuteDialogContext.Provider
-					value={{ mute: setUserIdToMute } as MuteDialogContextArgs}
-				>
-					<ChatMembers isManager={isManager} />
-				</MuteDialogContext.Provider>
-			</div>
-		</>
-	);
+  if (isAdmin === undefined || isOwner === undefined) return <></>;
+  return (
+    <>
+      <MuteDialog
+        userIdToMute={userIdToMute}
+        setUserIdToMute={setUserIdToMute}
+      />
+      <UpdateVisibilityDialog
+        showUpdateDialog={showUpdateDialog}
+        setShowUpdateDialog={setShowUpdateDialog}
+      />
+      <div className="room-param">
+        {isManager ? <AddUser /> : <>Members:</>}
+        {isOwner && (
+          <UpdateVisibilityButtons updateVisibility={setShowUpdateDialog} />
+        )}
+        <MuteDialogContext.Provider
+          value={{ mute: setUserIdToMute } as MuteDialogContextArgs}
+        >
+          <ChatMembers isManager={isManager} />
+        </MuteDialogContext.Provider>
+      </div>
+    </>
+  );
 }
