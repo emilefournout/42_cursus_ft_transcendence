bc8e29b82719b9614f36c22b8a21f7d63317e0e9
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-09-04 13:31:31 +0200

    Update vis btns dialog done.

--- frontend/src/pages/chat/room/param/RoomParam.tsx
+++ frontend/src/pages/chat/room/param/RoomParam.tsx
@@ -10,47 +10,49 @@ import { MuteDialog } from "./chat_members/MuteDialog";
 import { UpdateVisibilityDialog } from "./dialogs/UpdateVisibilityDialog";
 
 interface MuteDialogContextArgs {
-  mute: (value: number | undefined) => void;
+	mute: (value: number | undefined) => void;
 }
-export const MuteDialogContext = React.createContext(
-  {} as MuteDialogContextArgs
-);
-export function RoomParam() {
-  const roomContextArgs = useOutletContext<RoomContextArgs>();
-  const boardContext = useContext(BoardContext);
-  const me = roomContextArgs.chat.members?.find(
-    (member) => member.userId === boardContext?.me.id
-  );
-  const [showUpdateDialog, setShowUpdateDialog] = useState<boolean>(false);
-  const isOwner: boolean | undefined = me?.owner === true;
-  const isAdmin: boolean | undefined = me?.administrator === true;
-  const isManager = isOwner || isAdmin;
-  const [userIdToMute, setUserIdToMute] = useState<number | undefined>(
-    undefined
-  );
 
-  if (isAdmin === undefined || isOwner === undefined) return <></>;
-  return (
-    <>
-      <MuteDialog
-        userIdToMute={userIdToMute}
-        setUserIdToMute={setUserIdToMute}
-      />
-      <UpdateVisibilityDialog
-        showUpdateDialog={showUpdateDialog}
-        setShowUpdateDialog={setShowUpdateDialog}
-      />
-      <div className="room-param">
-        {isManager ? <AddUser /> : <div>members:</div>}
-        {isOwner && (
-          <UpdateVisibilityButtons updateVisibility={setShowUpdateDialog} />
-        )}
-        <MuteDialogContext.Provider
-          value={{ mute: setUserIdToMute } as MuteDialogContextArgs}
-        >
-          <ChatMembers isManager={isManager} />
-        </MuteDialogContext.Provider>
-      </div>
-    </>
-  );
+export const MuteDialogContext = React.createContext(
+	{} as MuteDialogContextArgs
+);
+
+export function RoomParam() {
+	const roomContextArgs = useOutletContext<RoomContextArgs>();
+	const boardContext = useContext(BoardContext);
+	const me = roomContextArgs.chat.members?.find(
+		(member) => member.userId === boardContext?.me.id
+	);
+	const [showUpdateDialog, setShowUpdateDialog] = useState<boolean>(false);
+	const isOwner: boolean | undefined = me?.owner === true;
+	const isAdmin: boolean | undefined = me?.administrator === true;
+	const isManager = isOwner || isAdmin;
+	const [userIdToMute, setUserIdToMute] = useState<number | undefined>(
+		undefined
+	);
+
+	if (isAdmin === undefined || isOwner === undefined) return <></>;
+	return (
+		<>
+			<MuteDialog
+				userIdToMute={userIdToMute}
+				setUserIdToMute={setUserIdToMute}
+			/>
+			<UpdateVisibilityDialog
+				showUpdateDialog={showUpdateDialog}
+				setShowUpdateDialog={setShowUpdateDialog}
+			/>
+			<div className="room-param">
+				{isManager ? <AddUser /> : <>Members:</>}
+				{isOwner && (
+					<UpdateVisibilityButtons updateVisibility={setShowUpdateDialog} />
+				)}
+				<MuteDialogContext.Provider
+					value={{ mute: setUserIdToMute } as MuteDialogContextArgs}
+				>
+					<ChatMembers isManager={isManager} />
+				</MuteDialogContext.Provider>
+			</div>
+		</>
+	);
 }
