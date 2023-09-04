f04b851f1e6fa35512a6d3af36829dc60c5a9e37
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-09-04 15:38:36 +0200

    Invitation and current game stuff.

--- frontend/src/pages/game/home_page/GameHomePage.tsx
+++ frontend/src/pages/game/home_page/GameHomePage.tsx
@@ -6,32 +6,34 @@ import { InvitationsColumns } from "./InvitationsColumns";
 import { WatchGameColumn } from "./WatchingGame/WatchGameColumn";
 
 export function GameHomePage() {
-  return (
-    <>
-      <SEO
-        title="Pong - Game"
-        description="Start a game with someone from the Internet with matchmaking or invite one of your friends."
-      />
+	return (
+		<>
+			<SEO
+				title="Pong - Game"
+				description="Start a game with someone from the Internet with matchmaking or invite one of your friends."
+			/>
 
-      <InvitationsColumns />
-      <WatchGameColumn />
-      <div className="wrapper-matchmaking">
-        <Link
-          className="btn game-creation-btn btn-top"
-          to="/board/game/matchmaking"
-        >
-          Matchmaking
-        </Link>
-        <div className="wrapper-row txt game-creation-divider">Or</div>
-        <Link
-          className="btn btn-bottom game-creation-btn"
-          to="/board/game/new-game"
-        >
-          Create a new game
-        </Link>
-      </div>
-      {/*separator or*/}
-      {/*Invite Friend Form*/}
-    </>
-  );
+			<div className="wrapper-matchmaking">
+				<Link
+					className="btn game-creation-btn btn-top"
+					to="/board/game/matchmaking"
+				>
+					Matchmaking
+				</Link>
+				<div className="wrapper-row txt game-creation-divider">Or</div>
+				<Link
+					className="btn btn-bottom game-creation-btn"
+					to="/board/game/new-game"
+				>
+					Create a new game
+				</Link>
+				<div className="wrapper-row">
+					<InvitationsColumns />
+					<WatchGameColumn />
+				</div>
+			</div>
+			{/*separator or*/}
+			{/*Invite Friend Form*/}
+		</>
+	);
 }
--- frontend/src/pages/game/home_page/WatchingGame/WatchGameColumn.tsx
+++ frontend/src/pages/game/home_page/WatchingGame/WatchGameColumn.tsx
@@ -2,34 +2,34 @@ import React, { useEffect, useState } from "react";
 import { WatchingGameCard } from "./WatchingGameCard";
 
 export function WatchGameColumn() {
-  const [activePlays, setActivePlays] = useState<Array<string>>([]);
+	const [activePlays, setActivePlays] = useState<Array<string>>([]);
 
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_BACKEND}/game/active-plays`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Error getting active plays");
-        }
-        return response.json();
-      })
-      .then((data: Array<string>) => {
-        setActivePlays(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-  return (
-    <>
-      <div>currents game:</div>
-      {activePlays.map((uuid) => {
-        return <WatchingGameCard key={uuid} />;
-      })}
-    </>
-  );
+	useEffect(() => {
+		fetch(`${process.env.REACT_APP_BACKEND}/game/active-plays`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+			},
+		})
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Error getting active plays");
+				}
+				return response.json();
+			})
+			.then((data: Array<string>) => {
+				setActivePlays(data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
+	return (
+		<div className="wrapper-col">
+			<div>currents game:</div>
+			{activePlays.map((uuid) => {
+				return <WatchingGameCard key={uuid} />;
+			})}
+		</div>
+	);
 }
