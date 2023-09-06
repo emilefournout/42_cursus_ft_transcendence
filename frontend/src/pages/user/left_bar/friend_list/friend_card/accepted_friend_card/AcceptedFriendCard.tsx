bd0643579c3663eed2df2a5b5d9ebee225aacc1e
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-09-06 16:53:42 +0200

    Watch game btn styling.

--- frontend/src/pages/user/left_bar/friend_list/friend_card/accepted_friend_card/AcceptedFriendCard.tsx
+++ frontend/src/pages/user/left_bar/friend_list/friend_card/accepted_friend_card/AcceptedFriendCard.tsx
@@ -7,37 +7,37 @@ import { useNavigate } from "react-router-dom";
 import { CardAction } from "./CardAction";
 
 export interface FriendCardProps {
-  userInfo: User;
+	userInfo: User;
 }
 
 export function AcceptedFriendCard(props: FriendCardProps) {
-  const navigate = useNavigate();
-  return (
-    <>
-      <div className="friend-card">
-        <div
-          className="friend-card-avatar"
-          onClick={() => navigate("/board/user-account/" + props.userInfo.id)}
-        >
-          <Avatar
-            url={props.userInfo.avatar}
-            size="48px"
-            upload={false}
-            download={true}
-          />
-        </div>
-        <div
-          className="friend-card-username ellipsed-txt"
-          onClick={() => navigate("/board/user-account/" + props.userInfo.id)}
-        >
-          {props.userInfo.username}
-        </div>
-        <FriendStatus
-          status={props.userInfo.status}
-          friendId={props.userInfo.id}
-        />
-        <CardAction userId={props.userInfo.id} />
-      </div>
-    </>
-  );
+	const navigate = useNavigate();
+	return (
+		<>
+			<div className="friend-card">
+				<div
+					className="friend-card-avatar"
+					onClick={() => navigate("/board/user-account/" + props.userInfo.id)}
+				>
+					<Avatar
+						url={props.userInfo.avatar}
+						size="48px"
+						upload={false}
+						download={true}
+					/>
+				</div>
+				<div
+					className="friend-card-username ellipsed-txt"
+					onClick={() => navigate("/board/user-account/" + props.userInfo.id)}
+				>
+					{props.userInfo.username}
+				</div>
+				<FriendStatus
+					status={props.userInfo.status}
+					friendId={props.userInfo.id}
+				/>
+				<CardAction userId={props.userInfo.id} />
+			</div>
+		</>
+	);
 }
