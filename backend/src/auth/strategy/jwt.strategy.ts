57126306fba249a506349da1056de8294a420cdd
José Manuel <josesanc@student.42malaga.com>
2023-09-07 13:42:12 +0200

    🥅 Internal server error on JWT validation fixed

--- backend/src/auth/strategy/jwt.strategy.ts
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -4,6 +4,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from '../interface/jwtpayload.dto';
 import { UserService } from 'src/user/user.service';
+import { UserServiceError } from 'src/user/exceptions/index.dto';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
@@ -18,12 +19,20 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   async validate(payload: JwtPayload) {
-    const user = await this.userService.findUserById(payload.sub);
-    if (!user) throw new UnauthorizedException('Token id not valid');
-    if (user.intraname != payload.intraname)
-      throw new UnauthorizedException(
-        'Username does not correspond to user identified by id'
-      );
-    return payload;
+    try {
+      const user = await this.userService.findUserById(payload.sub);
+      if (!user) throw new UnauthorizedException('Token id not valid');
+      if (user.intraname != payload.intraname)
+        throw new UnauthorizedException(
+          'Username does not correspond to user identified by id'
+        );
+      return payload;
+    } catch (error) {
+      if (error instanceof UserServiceError.UserNotFoundException) {
+        throw new UnauthorizedException('User does not exist');
+      } else {
+        throw error;
+      }
+    }
   }
 }
