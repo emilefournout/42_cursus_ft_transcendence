b8e9634c4874e4b22531d0a6d7317734542f519d
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-09-08 13:19:33 +0200

    Ongoing games column done.

--- frontend/src/pages/game/home_page/Invitations/InvitationCard.tsx
+++ frontend/src/pages/game/home_page/Invitations/InvitationCard.tsx
@@ -3,31 +3,32 @@ import { GameSocket } from "../../../../services/socket";
 import { useNavigate } from "react-router-dom";
 
 interface InvitationCardProps {
-  id: number;
-  username: string;
-  key: number;
+	id: number;
+	username: string;
+	key: number;
 }
+
 export function InvitationCard(props: InvitationCardProps) {
-  const gameSocket = GameSocket.getInstance().socket;
-  const navigate = useNavigate();
+	const gameSocket = GameSocket.getInstance().socket;
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    gameSocket.off("game_found");
-    gameSocket.on("game_found", (gameId) => {
-      navigate(`./${gameId}`);
-    });
-  }, [gameSocket, navigate]);
+	useEffect(() => {
+		gameSocket.off("game_found");
+		gameSocket.on("game_found", (gameId) => {
+			navigate(`./${gameId}`);
+		});
+	}, [gameSocket, navigate]);
 
-  return (
-    <div>
-      {props.username} invited you{" "}
-      <button
-        onClick={() => {
-          gameSocket.emit("join_private_room", props.id);
-        }}
-      >
-        accept
-      </button>
-    </div>
-  );
+	return (
+		<div>
+			{props.username} invited you{" "}
+			<button
+				onClick={() => {
+					gameSocket.emit("join_private_room", props.id);
+				}}
+			>
+				accept
+			</button>
+		</div>
+	);
 }
--- frontend/src/pages/game/home_page/WatchingGame/WatchingGameCard.tsx
+++ frontend/src/pages/game/home_page/WatchingGame/WatchingGameCard.tsx
@@ -4,65 +4,65 @@ import { useNavigate } from "react-router-dom";
 import { devlog } from "../../../../services/core";
 
 interface WatchingGameCardProps {
-  uuid: string;
-  key: string;
+	uuid: string;
+	key: string;
 }
 
 export function WatchingGameCard(props: WatchingGameCardProps) {
-  const [username_1, setUsername_1] = useState<string | undefined>(undefined);
-  const [username_2, setUsername_2] = useState<string | undefined>(undefined);
-  const navigate = useNavigate();
-  const fetch_username = (id: number) =>
-    fetch(`${process.env.REACT_APP_BACKEND}/user/info/${id}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Error getting user info");
-        }
-        return response.json();
-      })
-      .then((data: User) => data.username);
+	const [username_1, setUsername_1] = useState<string | undefined>(undefined);
+	const [username_2, setUsername_2] = useState<string | undefined>(undefined);
+	const navigate = useNavigate();
+    const fetch_username = (id: number) =>
+		fetch(`${process.env.REACT_APP_BACKEND}/user/info/${id}`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+			},
+		})
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Error getting user info");
+				}
+				return response.json();
+			})
+			.then((data: User) => data.username);
 
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_BACKEND}/game/info/${props.uuid}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Error getting game info");
-        }
-        return response.json();
-      })
-      .then((data: GameInfo) => {
-        fetch_username(data.user1_id)
-          .then((username) => setUsername_1(username))
-          .then(() => fetch_username(data.user2_id))
-          .then((username) => setUsername_2(username))
-          .catch((error) => {
-            devlog(error);
-          });
-      })
-      .catch((error) => {
-        devlog(error);
-      });
-  }, [props.uuid]);
+	useEffect(() => {
+		fetch(`${process.env.REACT_APP_BACKEND}/game/info/${props.uuid}`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+			},
+		})
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Error getting game info");
+				}
+				return response.json();
+			})
+			.then((data: GameInfo) => {
+				fetch_username(data.user1_id)
+					.then((username) => setUsername_1(username))
+					.then(() => fetch_username(data.user2_id))
+					.then((username) => setUsername_2(username))
+					.catch((error) => {
+						devlog(error);
+					});
+			})
+			.catch((error) => {
+				devlog(error);
+			});
+	}, [props.uuid]);
 
-  if (username_1 === undefined || username_2 === undefined) {
-    return <></>;
-  }
-  return (
-    <div>
-      <div>
-        {username_1} vs {username_2}
-      </div>
-      <button onClick={() => navigate(props.uuid)}>Watch Game</button>
-    </div>
-  );
+	if (username_1 === undefined || username_2 === undefined) {
+		return <></>;
+    }
+
+	return (
+		<div className="ongoing-game-card columns-txt" onClick={() => navigate(props.uuid)}>
+			<div className="ellipsed-txt">{username_1}</div>
+			vs
+			<div className="ellipsed-txt">{username_2}</div>
+		</div>
+	);
 }
