3e64c95e2ea233bfd2bb4e5e9057f72a2526b71b
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-09-10 14:16:26 +0200

    Achievement changes (and security?).

--- backend/sql/insertions.sql
+++ backend/sql/insertions.sql
@@ -1,7 +1,7 @@
 INSERT INTO "Achievement" (name, description)
 VALUES
     ('First Win', 'Win your first game'),
-    ('KO', 'Win a game without any goal to be scored'),
-    ('eSport trainee', 'Win ten games');
+    ('What A Player', 'Win a game without being scored a single point'),
+    ('Esport Trainee', 'Win ten games');
 
 INSERT INTO "User" (username, intraname) VALUES ('User Deleted', 'User Deleted')
--- frontend/src/pages/user/user_profile/profile/achievements/AchievementCard.tsx
+++ frontend/src/pages/user/user_profile/profile/achievements/AchievementCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./../ranking/Ranking.css";
+import notfoundIcon from "../../full_achievements/AchivementIcons/notfoundIcon.svg";
 import power from "../../full_achievements/AchivementIcons/PowerIcon.svg";
 import pill from "../../full_achievements/AchivementIcons/PillIcon.svg";
 import clock from "../../full_achievements/AchivementIcons/ClockIcon.svg";
@@ -19,8 +20,8 @@ export function AchievementCard(props: AchievementCardProps) {
 
 	const achDictionary:ADType = {
 		"First Win": ["Plain", power],
-		"KO": ["Weird", pill],
-		"eSport trainee": ["Supa Strange", clock]
+		"What A Player": ["Weird", pill],
+		"Esport Trainee": ["Supa Strange", clock]
 	};
 	const colourDict:CDType = {
 		"Plain": "var(--Trans-Light-Text-Extra)",
@@ -29,15 +30,15 @@ export function AchievementCard(props: AchievementCardProps) {
 		"Cryptid": "var(--Trans-Cosmic-Purple)"
 	};
 
-	const achTitle = props.title;
-	console.log(achDictionary[achTitle][1]);
+	const achColour = props.title in achDictionary && achDictionary[props.title][0] in colourDict ? colourDict[achDictionary[props.title][0]] : "var(--Trans-Paled-Beige)";
+	const achImg = props.title in achDictionary ? achDictionary[props.title][1] : notfoundIcon;
 
 	return (
 		<div className="ach-card-container">
-			<div className="ach-card-title ellipsed-txt" title={`${achTitle} achievement`}>{achTitle}</div>
+			<div className="ach-card-title ellipsed-txt" title={`${props.title} achievement`}>{props.title}</div>
 			<div className="ach-card-subtitle ellipsed-txt" title={props.description}>{props.description}</div>
-			<div className="ach-card-back" style={{backgroundColor: colourDict[achDictionary[props.title][0]]}}></div>
-			<img className="ach-card-img" src={achDictionary[achTitle][1]} alt={`${props.title} icon`}/>
+			<div className="ach-card-back" style={{backgroundColor: achColour}}></div>
+			<img className="ach-card-img" src={achImg} alt={`${props.title} icon`}/>
 		</div>
 	);
 }
