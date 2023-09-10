ffd56f4811edccd9957994815422b387d731b2a1
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-09-10 11:12:19 +0200

    Proc room dialog.

--- frontend/src/pages/chat/room/add/search/PasswordDialog.tsx
+++ frontend/src/pages/chat/room/add/search/PasswordDialog.tsx
@@ -3,48 +3,44 @@ import { ChatInfo } from "../../../Chat";
 import { devlog } from "../../../../../services/core";
 
 interface PasswordDialogProps {
-  showDialog: ChatInfo | undefined;
-  setShowDialog: (dialog: ChatInfo | undefined) => void;
-  fetchJoin: (chat: ChatInfo, password?: string) => Promise<void>;
+	showDialog: ChatInfo | undefined;
+	setShowDialog: (dialog: ChatInfo | undefined) => void;
+	fetchJoin: (chat: ChatInfo, password?: string) => Promise<void>;
 }
 
 export function PasswordDialog(props: PasswordDialogProps) {
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const closeDialog = () => props.setShowDialog(undefined);
-  const join = (password: string) => {
-    if (password.length === 0) {
-      setErrorMessage("Please enter a password");
-      return;
-    }
-    props.fetchJoin(props.showDialog!, password).catch((error) => {
-      devlog(error);
-      setErrorMessage("Incorrect password");
-    });
-  };
+	const [password, setPassword] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+	const closeDialog = () => props.setShowDialog(undefined);
+	const join = (password: string) => {
+		if (password.length === 0) {
+			setErrorMessage("Please enter a password");
+			return;
+		}
+		props.fetchJoin(props.showDialog!, password).catch((error) => {
+			devlog(error);
+			setErrorMessage("Incorrect password");
+		});
+	};
 
-  return (
-    <>
-      <dialog open={!!props.showDialog}>
-        {props.showDialog?.name} is protected, please enter password:
-        <input
-          value={password}
-          type="password"
-          placeholder="new password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            gap: "25px",
-          }}
-        >
-          <button onClick={closeDialog}>Close</button>
-          <button onClick={() => join(password)}>Join</button>
-        </div>
-        {errorMessage}
-      </dialog>
-    </>
-  );
+	return (
+		<>
+			{props.showDialog ? (
+				<dialog className="dialog-window wrapper-col" open={!!props.showDialog}>
+					{props.showDialog?.name} is protected, please enter password:
+					<input
+						value={password}
+						type="password"
+						placeholder="new password"
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<button onClick={() => join(password)}>Join</button>
+					<button onClick={closeDialog}>Close</button>
+					{errorMessage}
+				</dialog>
+			) : (
+				<></>
+			)}
+		</>
+	);
 }
